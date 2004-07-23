093b11e039f40e5e42037d8bc401245dab0a8a47
Jean-Philippe Chancelier <jpc@cermics.enpc.fr>
2004-07-23 16:03:02 +0000

    update

--- src/include/nsp/list-in.h
+++ src/include/nsp/list-in.h
@@ -9,25 +9,4 @@
 
 #include "nsp/interf.h"
 
-extern function int_lxret ;
-extern function int_lxvirg ;
-extern function int_lxpvirg ;
-extern function int_lxlist ;
-extern function int_lxextract1 ;
-extern function int_lxextract ;
-extern function int_lxextractall ;
-extern function int_lxpath ;
-extern function int_lxsetrc ;
-extern function int_lxcreate ;
-extern function int_lxdestroy ;
-extern function int_lxcopy ;
-extern function int_lxnthel ;
-extern function int_lxendi ;
-extern function int_lxni ;
-extern function int_lxnthdel ;
-extern function int_lxlength ;
-extern function int_lxconcat ;
-extern function int_lxinfo ;
-extern function int_lxprint ;
-
 #endif
