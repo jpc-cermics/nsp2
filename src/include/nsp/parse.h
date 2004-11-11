5021d930df9b7179fda728aa079f3087939083bf
Jean-Philippe Chancelier <jpc@cermics.enpc.fr>
2004-11-11 14:53:18 +0000

    update

--- src/include/nsp/parse.h
+++ src/include/nsp/parse.h
@@ -6,7 +6,6 @@ extern int TokenLineSet(int l);
 int ParseEvalFile(char *Str, int display);
 int ParseEvalFromStr(char *Str, int display);
 int ParseEvalDir(char *Dir, char *Fname);
-int ParseEvalLoop(int display);
 int ParseEvalFromStd(int display);
 
 #endif 
