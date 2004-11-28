0ab6939b1baf0c33b2223cd448bb54fe0224cbbe
Jean-Philippe Chancelier <jpc@cermics.enpc.fr>
2004-11-28 08:39:15 +0000

    update

---
+++ src/include/nsp/matint.h
@@ -0,0 +1,34 @@
+/* -*- Mode: C -*- */
+#ifndef INC_NSP_Matint
+#define INC_NSP_Matint
+
+/*********************************************************************
+ * This Software is ( Copyright ENPC/CERMICS 1998-2005) 
+ * Jean-Philippe Chancelier Enpc/Cermics 
+ *********************************************************************/
+  
+/* interface Matint */
+
+#include "nsp/object.h"
+
+typedef int matint_redim(void *M,int m,int n); 
+
+
+typedef struct _nsp_type_Matint {
+  NSP_TYPE_OBJECT__
+  /* added */
+  matint_redim *redim; 
+} NspTypeMatint ; 
+
+#define MAT_INT(t) ((NspTypeMatint *) t)
+
+extern int nsp_type_matint_id;
+extern NspTypeMatint *nsp_type_matint;
+
+NspTypeMatint *new_type_matint(type_mode mode);
+
+#endif 
+
+
+
+
