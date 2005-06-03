842e57cdea38c938e34c113eeefd16ce65d568e7
Jean-Philippe Chancelier <jpc@cermics.enpc.fr>
2005-06-03 14:58:23 +0000

    bug

--- src/objects/qsort.c
+++ src/objects/qsort.c
@@ -94,7 +94,7 @@ void nsp_qsort(char *a, char *tab, int flag, int n, int es, int es1, int (*cmp) 
   char *pa, *pb, *pc, *pd, *pl, *pm, *pn;
   char *taba, *tabb, *tabc, *tabd, *tabl, *tabm, *tabn;
   int d,dind, r,r1,  swap_cnt;
-  int ndes=n/es;ndes1=n/es1;
+  int ndes=n/es,ndes1=n/es1;
  loop:	
   swap_cnt = 0;
   if (n < 7) {
