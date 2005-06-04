5a238e6d92f75832cc4d8dbd01976312f58d45cd
Jean-Philippe Chancelier <jpc@cermics.enpc.fr>
2005-06-04 14:34:25 +0000

    addinter test added

---
+++ tests/addinter.c
@@ -0,0 +1,94 @@
+/* Nsp
+ * Copyright (C) 1998-2005 Jean-Philippe Chancelier Enpc/Cermics
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+/*
+ * Interface demo file 
+ */
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "nsp/interf.h"
+
+static void f99(NspMatrix *M);
+
+int int_first(Stack stack, int rhs, int opt, int lhs)
+{
+  NspMatrix *A;
+  CheckRhs(1,1);
+  CheckLhs(1,1);
+  if ((A = GetMatCopy(stack,1)) == NULLMAT)  return RET_BUG;
+  f99(A);
+  NSP_OBJECT(A)->ret_pos = 1;
+  return 1;
+}
+
+static void f99(NspMatrix *M)
+{
+  int i;
+  if ( M->rc_type == 'r' ) 
+    for ( i= 0 ; i < M->mn ; i++) M->R[i] *= 2.0;
+  else 
+    for ( i= 0 ; i < M->mn ; i++) { M->C[i].r *= 2.0;M->C[i].i *= 3.0;}
+}
+
+static void not(NspBMatrix *B) ;
+
+int int_second(Stack stack, int rhs, int opt, int lhs)
+{
+  NspBMatrix *A;
+  CheckRhs(1,1);
+  CheckLhs(1,1);
+  /* since not modify its argument we must use a copy */ 
+  if ((A = GetBMatCopy(stack,1)) == NULLBMAT)  return RET_BUG;
+  not(A);
+  NSP_OBJECT(A)->ret_pos = 1;
+  return 1; 
+}
+
+static void not(NspBMatrix *B) 
+{
+  int i;
+  for ( i=0 ; i < B->mn ; i++)  B->B[i] = !  B->B[i];
+}
+
+static OpTab libaddinter_func[]={
+  {"first",int_first},
+  {"second",int_second},
+  {(char *) 0, NULL}
+};
+
+int libaddinter_Interf(int i, Stack stack, int rhs, int opt, int lhs)
+{
+  return (*(libaddinter_func[i].fonc))(stack,rhs,opt,lhs);
+}
+
+/* used to walk through the interface table 
+ * (for adding or removing functions) 
+ */
+
+void libaddinter_Interf_Info(int i, char **fname, function (**f))
+{
+  *fname = libaddinter_func[i].name;
+  *f = libaddinter_func[i].fonc;
+}
+
+
+
---
+++ tests/addinter.tst
@@ -0,0 +1,52 @@
+// -*- Mode: scilab -*-
+// Copyright (C) 2005 J.P Chancelier Cermics/Enpc
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ 
+// basic test for link and addinter 
+
+// create a shared library 
+rep=system('../bin/nsplibtool OBJS=addinter.o LIBRARY=libaddinter > /dev/null');
+if rep<> 0 then pause;end 
+// link shared library 
+if ~c_link('libaddinter_Interf') then 
+   ilib=addinter('./libaddinter.'+ilib_soname()','libaddinter');
+else 
+   error('libaddinter already loaded');
+end
+// test that first and second work
+if first(1:5)<>2*(1:5) then pause;end
+if second([%t,%f])<>~[%t,%f] then pause,end
+// get all the entry points 
+H=link();
+// check that the interface is present in H
+if ~H.iskey['libaddinter_Interf'] then pause;end
+if H('libaddinter_Interf')<>ilib then pause;end
+// dlclose the shared archive 
+ulink(ilib)
+// clean files 
+rep=system('../bin/nsplibtool OBJS=addinter.o LIBRARY=libaddinter distclean > /dev/null');
+
+// test that shared archive is unlinked 
+if c_link('libaddinter_Interf') then pause,end
+
+// check if we can access nsp symbols 
+// with dynamic linking.
+// 
+rep=execstr('ilib=link(''nsp'',''nsp_matrix_print'')',errcatch=%t);
+if rep == %f then pause,end
+// access other symbols through ilib 
+link(ilib,'nsp_matrix_info');
+ulink(ilib);
