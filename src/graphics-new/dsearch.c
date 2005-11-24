43315a1582b816ae793b103b2171a70c68dbd826
Bruno Pinçon <bruno.pincon@univ-lorraine.fr>
2005-11-24 13:37:20 +0000

    add dsearch for strings

--- src/graphics-new/dsearch.c
+++ src/graphics-new/dsearch.c
@@ -9,6 +9,7 @@
 #include <string.h> 
 
 #include "nsp/matrix-in.h"
+#include "nsp/smatrix-in.h"
 #include "nsp/bmatrix-in.h"
 #include "../interp/Eval.h"
 
@@ -20,6 +21,13 @@ void nsp_dsearchc(const double x[], int m,const double val[], int n,
 
 void nsp_dsearchd(const double x[], int m,const double val[], int n,
 		  int indx[], int occ[], int *info);
+
+void nsp_dsearchc_for_strings(char **x, int m, char **val, int n, 
+			      int indx[], int occ[], int *info);
+
+void nsp_dsearchd_for_strings(char **x, int m, char **val, int n,
+			      int *indx, int *occ, int *info);
+
 
 /*
  * dsearch interface  
@@ -39,48 +47,81 @@ void nsp_dsearchd(const double x[], int m,const double val[], int n,
 int int_dsearch(Stack stack, int rhs, int opt, int lhs)
 {
   char *type;
-  int info, m_occ,n_occ,i;
+  int info, m_occ, n_occ, m_x, n_x, mn_val, i, flagstr;
   char c= 'c';
-  NspMatrix *x, *val, *ind, *occ;
+  NspMatrix *x=NULLMAT, *val=NULLMAT, *ind=NULLMAT, *occ=NULLMAT;
+  NspSMatrix *xstr=NULLSMAT, *valstr=NULLSMAT;
   CheckRhs(2,3);
   CheckLhs(0,3);
-  if ((x =GetRealMat(stack,1))== NULLMAT ) return RET_BUG;
-  /* CheckVector(stack.fname,1,x);*/
-  if ((val =GetRealMat(stack,2))== NULLMAT ) return RET_BUG;
-  CheckVector(stack.fname,2,val);
+
+  if ( IsMatObj(stack,1) ) 
+    {
+      if ( (x=GetRealMat(stack,1)) == NULLMAT ) return RET_BUG;
+      if ( (val=GetRealMat(stack,2)) == NULLMAT ) return RET_BUG;
+      CheckVector(stack.fname,2,val);
+      flagstr = 0; m_x = x->m; n_x = x->n;
+      m_occ = val->m;  n_occ = val->n; mn_val = val->mn;
+    }
+  else if ( IsSMatObj(stack,1) ) 
+    {
+      if ( (xstr=GetSMat(stack,1)) == NULLSMAT ) return RET_BUG;
+      if ( (valstr=GetSMat(stack,2)) == NULLSMAT ) return RET_BUG;
+      CheckVector(stack.fname,2,valstr);
+      flagstr = 1; m_x = xstr->m; n_x = xstr->n;
+      m_occ = valstr->m; n_occ = valstr->n; mn_val = valstr->mn;
+    }
+  else
+    {
+      Scierror("Error: first argument must be of type real or string in function %s\n",stack.fname);
+      return RET_BUG;
+    }
+
   if ( rhs == 3 ) 
     {
       if ((type = GetString(stack,3)) == NULLSTRING ) return RET_BUG;
       if ( strcmp(type,"c") != 0 && strcmp(type,"d") != 0 ) 
 	{
-	  Scierror("Error: wrong second argument in function %s\n",stack.fname);
+	  Scierror("Error: wrong third argument in function %s\n",stack.fname);
 	  Scierror("\tonly 'c' or 'd' are allowed\n");
 	  return RET_BUG;
 	}
       c = type[0];
     }
-  if (( ind = nsp_matrix_create(NVOID,'r',x->m,x->n)) == NULLMAT ) return RET_BUG;
-  m_occ = val->m ; 
-  n_occ = val->n ;
+
+  if ( (ind=nsp_matrix_create(NVOID,'r',m_x,n_x)) == NULLMAT ) return RET_BUG;
   if ( c == 'c' )
     {
       m_occ = Max(m_occ -1, 1); 
       n_occ = Max(n_occ -1, 1);
     }
-  if (( occ = nsp_matrix_create(NVOID,'r',m_occ,n_occ)) == NULLMAT ) return RET_BUG;
+
+  if ( (occ=nsp_matrix_create(NVOID,'r',m_occ,n_occ)) == NULLMAT ) return RET_BUG;
+
   /* Check that val is increasing */
-  for ( i = 0 ; i < val->mn -1 ; i++ ) 
+  if ( flagstr == 0 )
     {
-      if ( ! ( val->R[i] < val->R[i+1]) )
-	{
-	  Scierror("Error: second argument in function %s\n",stack.fname);
-	  Scierror("\tis not scrictly increasing \n");
-	  return RET_BUG;
-	}
+      for ( i = 0 ; i < mn_val-1 ; i++ ) 
+	if ( ! ( val->R[i] < val->R[i+1]) )
+	  {
+	    Scierror("Error: second argument in function %s\n",stack.fname);
+	    Scierror("\tis not scrictly increasing \n");
+	    return RET_BUG;
+	  }
     }
+  else
+    {
+      for ( i = 0 ; i < mn_val-1 ; i++ ) 
+	if ( strcmp(valstr->S[i],valstr->S[i+1]) >= 0 )
+	  {
+	    Scierror("Error: second argument in function %s\n",stack.fname);
+	    Scierror("\tis not scrictly increasing \n");
+	    return RET_BUG;
+	  }
+    }
+
   if ( c == 'c' ) 
     {
-      if ( val->mn <= 1 ) 
+      if ( mn_val <= 1 ) 
 	{ 
 	  Scierror("%s: second argument should be of size > 1 for 'c' option \n",stack.fname);
 	  return RET_BUG;
@@ -88,16 +129,28 @@ int int_dsearch(Stack stack, int rhs, int opt, int lhs)
     }
   else
     {
-      if ( val->mn < 1 ) 
+      if ( mn_val < 1 ) 
 	{ 
 	  Scierror("%s: second argument should be of size >= 1 for 'd' option \n",stack.fname);
 	  return RET_BUG;
 	}
     }
-  if ( c == 'c' ) 
-    nsp_dsearchc(x->R,x->mn,val->R,val->mn-1,ind->I,occ->I,&info);
-  else 
-    nsp_dsearchd(x->R,x->mn,val->R,val->mn,ind->I,occ->I,&info);
+
+  if ( flagstr == 0 )
+    {
+      if ( c == 'c' ) 
+	nsp_dsearchc(x->R,x->mn,val->R,val->mn-1,ind->I,occ->I,&info);
+      else 
+	nsp_dsearchd(x->R,x->mn,val->R,val->mn,ind->I,occ->I,&info);
+    }
+  else
+    {
+      if ( c == 'c' ) 
+	nsp_dsearchc_for_strings(xstr->S,xstr->mn,valstr->S,mn_val-1,ind->I,occ->I,&info);
+      else 
+	nsp_dsearchd_for_strings(xstr->S,xstr->mn,valstr->S,mn_val,ind->I,occ->I,&info);
+    }
+
   ind->convert = 'i'; /* occ is filed with integers */
   ind = Mat2double(ind);
   MoveObj(stack,1,NSP_OBJECT(ind));
@@ -125,13 +178,13 @@ int int_dsearch(Stack stack, int rhs, int opt, int lhs)
  *        dichotomic search, computes : 
  *
  *           a/ for each x[i] its interval number stored in indx[i] : 
- *                     indx[i] = j if  x[i] in (val[j-1], val[j] ] 
- *                             = 1 if  x[i] = val[0]
- *                             = 0 if  x[i] is not in [val(0),val(n)] 
+ *                  I(j) = [val[j-1], val[j] )  j=1,...,n-1 
+ *                  I(n) = [val[n-1], val[n] ]
+ *               indx[i] = 0 if  x[i] is not in [val(1),val(n+1)] 
  *
  *           b/ the number of points falling in the interval j : 
  *
- *              occ[j] = # { x[i] such that x[i] in ( val[j], val[j+1] } 
+ *              occ[j] = # { x[i] such that x[i] in I(j) } 
  *
  *     PARAMETERS 
  *        inputs : 
@@ -161,27 +214,24 @@ void nsp_dsearchc(const double x[], int m,const double val[], int n,
     {
       if (val[0] <= x[i] && x[i] <= val[n]) 
 	{
-	  /* x[i] is in [val(0),val(n)] : 
-	   * find j such that val[j-1] <= x[i] <= val[j] by a dicho search 
-	   */
+	  /* x[i] is in [val(0),val(n)]:  find j such that x[i] is in I(j) */
 	  j1 = 0;
 	  j2 = n;
-	  while(j2 - j1 > 1) {
-	    j = (j1 + j2) / 2;
-	    if (x[i] <= val[j]) {
-	      j2 = j;
-	    } else {
-	      j1 = j;
+	  while(j2 - j1 > 1) 
+	    {
+	      j = (j1 + j2) / 2;
+	      if (x[i] < val[j]) j2 = j; else j1 = j;
 	    }
-	  }
-	  /* we have val[j1] < x[i] <= val[j2]  if j2 > 1  (j1=j2-1) 
-	   * or val[j1] <= x[i] <= val[j2] if j2 = 1  (j1=j2-1) 
-	   * so that j2 is the good interval number in all cases 
+	  /* here we have : (i)  j2 = j1+1 
+	   *                (ii) val[j1] <= x[i] < val[j2]  if j2 < n 
+           *                  or val[j1] <= x[i] <= val[j2] if j2 = n
+	   * so j2 is the good interval number in all cases 
 	   */
 	  ++occ[j1]; 
 	  indx[i] = j2 ;
-	} else {
-	  /* x[i] is not in [val(0), val(n)] */
+	} 
+      else   /* x[i] is not in [val(0), val(n)] */ 
+	{
 	  ++(*info);
 	  indx[i] = 0;
 	}
@@ -222,44 +272,126 @@ void nsp_dsearchd(const double *x, int m,const double *val, int n,
 
   for (i = 0 ; i < m ; i++) 
     {
-      if (val[0] <= x[i] && x[i] <= val[n-1]) {
-	/* find j such that x[i] = val(j) by a dicho search */
-	j1 = 0;
-	j2 = n-1;
-	while(j2 - j1 > 1) {
-	  j = (j1 + j2) / 2;
-	  if (x[i] < val[j]) {
-	    j2 = j;
-	  } else {
-	    j1 = j;
-	  }
-	}
-	/*  here we know that val(j1) <= x[i] <= val(j2)  with j2 = j1 + 1
-	 *  (in fact we have exactly  val(j1) <= x[i] < val(j2) if j2 < n) 
-	 */
-	if (x[i] == val[j1]) {
-	  ++occ[j1];
-	  indx[i] = j1+1;
-	} else if (x[i] == val[j2]) {
-	  /* (note: this case may hap */
-	  ++occ[j2];
-	  indx[i] = j2+1;
-	} else {
-	  /* x[i] is not in {val(1), val(2),..., val(n)} */
+      if (val[0] <= x[i] && x[i] <= val[n-1]) 
+	{
+	  /* find j such that x[i] = val(j) by a dicho search */
+	  j1 = 0;
+	  j2 = n-1;
+	  while(j2 - j1 > 1) 
+	    {
+	      j = (j1 + j2) / 2;
+	      if (x[i] < val[j]) j2 = j; else j1 = j;
+	    }
+	  /*  here we know that val(j1) <= x[i] <= val(j2)  with j2 = j1 + 1
+	   *  (in fact we have exactly  val(j1) <= x[i] < val(j2) if j2 < n) 
+	   */
+	  if (x[i] == val[j1]) 
+	    {
+	      ++occ[j1];
+	      indx[i] = j1+1;
+	    } 
+	  else if (x[i] == val[j2]) /* (note: this case may happen only for j2=n-1) */
+	    {
+	      ++occ[j2];
+	      indx[i] = j2+1;
+	    } 
+	  else  /* x[i] is not in {val(1), val(2),..., val(n)} */ 
+	    {
+	      ++(*info);
+	      indx[i] = 0;
+	    }
+	} 
+      else    /* x[i] is not in {val(1), val(2),..., val(n)} */ 
+	{
 	  ++(*info);
 	  indx[i] = 0;
 	}
-      } else {
-	/* x[i] is not in {val(1), val(2),..., val(n)} */
-	++(*info);
-	indx[i] = 0;
-      }
     }
 } 
 
 
+void nsp_dsearchc_for_strings(char **x, int m, char **val, int n, 
+			      int indx[], int occ[], int *info)
+{
+  int  i, j, j1, j2;
+ 
+  for (j = 0; j < n; j++ ) occ[j]= 0;
+  *info = 0;
+
+  for (i = 0; i < m ; i++ ) 
+    {
+      if ( strcmp(val[0],x[i]) <= 0 &&  strcmp(x[i],val[n]) <= 0 ) 
+	{
+	  /* x[i] is in [val(0),val(n)] :  find j such that x[i] is in I(j) */
+	  j1 = 0; j2 = n;
+	  while(j2 - j1 > 1) 
+	    {
+	      j = (j1 + j2) / 2;
+	      if ( strcmp(x[i],val[j]) < 0 ) j2 = j; else j1 = j;
+	    }
+	  /* here we have : (i)  j2 = j1+1 
+	   *                (ii) val[j1] <= x[i] < val[j2]  if j2 < n 
+           *                  or val[j1] <= x[i] <= val[j2] if j2 = n
+	   * so j2 is the good interval number in all cases 
+	   */
+	  ++occ[j1]; 
+	  indx[i] = j2 ;
+	} 
+      else  /* x[i] is not in [val(0), val(n)] */ 
+	{
+	  ++(*info);
+	  indx[i] = 0;
+	}
+    }
+}
 
 
+void nsp_dsearchd_for_strings(char **x, int m, char **val, int n,
+			      int *indx, int *occ, int *info)
+{
+  int i, j, j1, j2;
+
+  for (j = 0; j < n ; j++ ) occ[j]= 0;
+  *info = 0;
+
+  for (i = 0 ; i < m ; i++) 
+    {
+      if ( strcmp(val[0],x[i]) <= 0  &&  strcmp(x[i],val[n-1]) <= 0 ) 
+	{
+	  /* find j such that x[i] = val(j) by a dicho search */
+	  j1 = 0;
+	  j2 = n-1;
+	  while(j2 - j1 > 1) 
+	    {
+	      j = (j1 + j2) / 2;
+	      if ( strcmp(x[i],val[j]) < 0 ) j2 = j; else j1 = j;
+	    }
+	  /*  here we know that val(j1) <= x[i] <= val(j2)  with j2 = j1 + 1
+	   *  (in fact we have exactly  val(j1) <= x[i] < val(j2) if j2 < n-1) 
+	   */
+	  if ( strcmp(x[i],val[j1]) == 0 ) 
+	    {
+	      ++occ[j1];
+	      indx[i] = j1+1;
+	    } 
+	  else if ( j2 == n-1  &&  strcmp(x[i],val[j2]) == 0 )  /* this case may happen only for j2=n-1 */
+	    {
+	      ++occ[j2];
+	      indx[i] = j2+1;
+	    } 
+	  else /* x[i] is not in {val(1), val(2),..., val(n)} */ 
+	    {
+	      ++(*info);
+	      indx[i] = 0;
+	    }
+	} 
+      else     /* x[i] is not in {val(1), val(2),..., val(n)} */ 
+	{
+	  ++(*info);
+	  indx[i] = 0;
+	}
+    }
+} 
 
 
 
