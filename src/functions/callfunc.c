6beeed67cbc58c2141f4248b8cfd1ec305c14a61
Jean-Philippe Chancelier <jpc@cermics.enpc.fr>
2006-07-23 18:51:00 +0000

    sparse reorganized

--- src/functions/callfunc.c
+++ src/functions/callfunc.c
@@ -46,10 +46,10 @@ extern  sci_interface  Obj_Interf  ;extern  interface_info  Obj_Interf_Info  ;
 extern  sci_interface  File_Interf  ;extern  interface_info  File_Interf_Info  ;
 extern  sci_interface  Parse_Interf ;extern  interface_info  Parse_Interf_Info ;
 extern  sci_interface  SpMatrix_Interf  ;extern  interface_info  SpMatrix_Interf_Info  ;
+extern  sci_interface  SpColMatrix_Interf  ;extern  interface_info  SpColMatrix_Interf_Info  ;
 extern  sci_interface  Functions_Interf  ;extern  interface_info  Functions_Interf_Info  ;
 extern  sci_interface  Function_Interf  ;extern  interface_info  Function_Interf_Info  ;
 extern  sci_interface  mex_Interf  ;extern  interface_info  mex_Interf_Info  ;
-extern  sci_interface  SpMatOps_Interf  ;extern  interface_info  SpMatOps_Interf_Info  ;
 extern  sci_interface  Interf_Interf  ;extern  interface_info  Interf_Interf_Info  ;
 extern  sci_interface  Datas_Interf ;extern  interface_info  Datas_Interf_Info ;
 extern  sci_interface  Graphics_Interf ;extern  interface_info  Graphics_Interf_Info ;
@@ -116,11 +116,11 @@ InterfTab Interfaces[]={
   {SMatrix_Interf,SMatrix_Interf_Info},
   {File_Interf,File_Interf_Info},
   {SpMatrix_Interf,SpMatrix_Interf_Info},
+  /*  {SpColMatrix_Interf,SpColMatrix_Interf_Info}, */
   {Parse_Interf,Parse_Interf_Info},
   {Functions_Interf,Functions_Interf_Info},
   {Function_Interf,Function_Interf_Info},
   {mex_Interf,mex_Interf_Info},
-  {SpMatOps_Interf,SpMatOps_Interf_Info},
   {Interf_Interf,Interf_Interf_Info},
   {Datas_Interf,Datas_Interf_Info},
   {Graphics_Interf,Graphics_Interf_Info},
