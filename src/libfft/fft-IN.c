684df1ec0c232d5acbf22e6ce9eb7c5e6ed0105b
Bruno Pinçon <bruno.pincon@univ-lorraine.fr>
2006-11-16 08:20:07 +0000

    add fftshit and ifftshift + use new func of GetDimArg

--- src/libfft/fft-IN.c
+++ src/libfft/fft-IN.c
@@ -59,8 +59,7 @@ int int_nsp_fft( Stack stack, int rhs, int opt, int lhs)
    */
   NspMatrix *x;
   double invn=0;
-  int s, k;
-  char *str;
+  int s, k, dim_flag=0;
   static fftw_plan forward_plan=NULL, backward_plan=NULL, multi_plan;
   static int last_forward_n=-1, last_backward_n=-1;
 
@@ -86,22 +85,17 @@ int int_nsp_fft( Stack stack, int rhs, int opt, int lhs)
     }
 
   if ( rhs == 3 )
-    {
-      if ((str = GetString(stack,3)) == (char*)0)
-	return RET_BUG;
-    }
-  else
-    str = "f";
-
+    if ( GetDimArg(stack, 3, &dim_flag) == FAIL )
+      return RET_BUG;
 
   if ((x = GetMatCopy(stack,1)) == NULLMAT) 
     return RET_BUG;
 
   if ( x->rc_type == 'r' )
-    if (nsp_mat_set_ival(x,0.00) == FAIL ) 
+    if (nsp_mat_complexify(x,0.00) == FAIL ) 
       return RET_BUG;
 
-  if ( str[0] == '*' || str[0] == 'f'  || str[0] == 'F' )
+  if ( dim_flag == 0 )
     {
       if ( s == -1 ) /* forward fft */
 	{
@@ -114,7 +108,7 @@ int int_nsp_fft( Stack stack, int rhs, int opt, int lhs)
 	    }
 	  fftw_execute_dft(forward_plan, (fftw_complex *)x->C , (fftw_complex *)x->C);
 	}
-      else  /* backward fft */
+      else           /* backward fft */
 	{
 	  if ( x->mn != last_backward_n )
 	    {  
@@ -127,36 +121,34 @@ int int_nsp_fft( Stack stack, int rhs, int opt, int lhs)
 	  invn = 1.0 / x->mn;
 	}
     }
-  else   /* fft of the rows or the columns */
+  else if ( dim_flag == 1 )      /* fft of the columns */
     {
-      if ( str[0] == 'c' || str[0] == 'C' )  /* fft of the columns */
-	{
-	  if ( s == 1) invn = 1.0 / x->m;
-	  multi_plan = fftw_plan_many_dft(1, &(x->m), x->n,  
-					 (fftw_complex *)x->C, &(x->mn), 1, x->m,
-					 (fftw_complex *)x->C, &(x->mn), 1, x->m,
-					 s, FFTW_ESTIMATE | FFTW_UNALIGNED );
-	}
-      else if ( str[0] == 'r' || str[0] == 'R' ) /* fft of the rows */
-	{
-	  if ( s == 1) invn = 1.0 / x->n;
-	  multi_plan = fftw_plan_many_dft(1, &(x->n), x->m,  
-					 (fftw_complex *)x->C, &(x->mn), x->m, 1,
-					 (fftw_complex *)x->C, &(x->mn), x->m, 1,
-					 s, FFTW_ESTIMATE | FFTW_UNALIGNED );
-	}
-      else
-	{
-	  Scierror("%s: bad third arg\n",NspFname(stack));
-	  return RET_BUG;
-	}
-      fftw_execute(multi_plan);
-      fftw_destroy_plan(multi_plan);
+      if ( s == 1) invn = 1.0 / x->m;
+      multi_plan = fftw_plan_many_dft(1, &(x->m), x->n,  
+				      (fftw_complex *)x->C, &(x->mn), 1, x->m,
+				      (fftw_complex *)x->C, &(x->mn), 1, x->m,
+				      s, FFTW_ESTIMATE | FFTW_UNALIGNED );
     }
+  else if ( dim_flag == 2 )      /* fft of the rows */
+    {
+      if ( s == 1) invn = 1.0 / x->n;
+      multi_plan = fftw_plan_many_dft(1, &(x->n), x->m,  
+				      (fftw_complex *)x->C, &(x->mn), x->m, 1,
+				      (fftw_complex *)x->C, &(x->mn), x->m, 1,
+				      s, FFTW_ESTIMATE | FFTW_UNALIGNED );
+    }
+  else
+    {
+      Scierror("%s: Invalid dim flag '%d' (must be 0, 1 or 2)\n", NspFname(stack), dim_flag);
+      return RET_BUG;
+    }
+
+  fftw_execute(multi_plan);
+  fftw_destroy_plan(multi_plan);
 
   if ( s == 1 ) /* backward fft => apply normalisation */
     for (k = 0 ; k < x->mn ; k++) { x->C[k].r *= invn; x->C[k].i *= invn; }
-
+  
   NSP_OBJECT (x)->ret_pos = 1;
   return 1;
 }
@@ -197,7 +189,7 @@ int int_nsp_fft2( Stack stack, int rhs, int opt, int lhs)
     return RET_BUG;
 
   if ( x->rc_type == 'r' )
-    if (nsp_mat_set_ival(x,0.00) == FAIL ) 
+    if (nsp_mat_complexify(x,0.00) == FAIL ) 
       return RET_BUG;
 
   p = fftw_plan_dft_2d(x->n, x->m, (fftw_complex *)x->C, (fftw_complex *)x->C, s, FFTW_ESTIMATE);
@@ -225,8 +217,7 @@ int int_nsp_fft( Stack stack, int rhs, int opt, int lhs)
    */
   enum {ALL, BY_ROW, BY_COL};
   NspMatrix *x, *y;
-  int s, k;
-  char *str;
+  int s, k, dim_flag=0;
   static double *wsave = NULL, invn;
   static int last_n=-1, n, flag_orient;
 
@@ -252,39 +243,34 @@ int int_nsp_fft( Stack stack, int rhs, int opt, int lhs)
     }
 
   if ( rhs == 3 )
-    {
-      if ((str = GetString(stack,3)) == (char*)0)
-	return RET_BUG;
-    }
-  else
-    str = "f";
-
+    if ( GetDimArg(stack, 3, &dim_flag) == FAIL )
+      return RET_BUG;
 
   if ((x = GetMatCopy(stack,1)) == NULLMAT) 
     return RET_BUG;
 
   if ( x->rc_type == 'r' )
-    if (nsp_mat_set_ival(x,0.00) == FAIL ) 
+    if (nsp_mat_complexify(x,0.00) == FAIL ) 
       return RET_BUG;
 
-  if ( str[0] == '*' || str[0] == 'f'  || str[0] == 'F' )
+  if ( dim_flag == 0 )
     {
       n = x->mn;
       flag_orient = ALL;
     }
-  else if ( str[0] == 'c' ||  str[0] == 'C' )
+  else if ( dim_flag == 1 )
     {
       n = x->m;
       flag_orient = BY_COL;
     }
-  else if ( str[0] == 'r' ||  str[0] == 'R' )
+  else if ( dim_flag == 2 )
     {
       n = x->n;
       flag_orient = BY_ROW;
     }
   else
     {
-      Scierror("%s: bad third arg\n",NspFname(stack));
+      Scierror("%s: Invalid dim flag '%d' (must be 0, 1 or 2)\n", NspFname(stack), dim_flag);
       return RET_BUG;
     }
 
@@ -425,10 +411,164 @@ int int_nsp_fft2( Stack stack, int rhs, int opt, int lhs)
 
 #endif
 
+static NspMatrix *nsp_mat_shift(NspMatrix *x, int dim_flag, char orient)
+{
+  NspMatrix *y;
+  int j, im, jm;
+  Boolean is_vector;
+
+  if ( (y = nsp_matrix_create(NVOID, x->rc_type, x->m, x->n)) == NULLMAT )
+    return NULLMAT;
+
+  is_vector =  x->m == 1 || x->n == 1;
+
+  if ( dim_flag == 0 )
+    {
+      if ( is_vector )
+	{
+	  im = orient == 's' ? (int) ceil( x->mn/2.0 ) : (int) floor( x->mn/2.0 );
+	  if ( y->rc_type == 'r' )
+	    {
+	      memcpy(&y->R[x->mn-im], x->R, im*sizeof(double));
+	      memcpy(y->R, &x->R[im], (x->mn-im)*sizeof(double));
+	    }
+	  else
+	    {
+	      memcpy(&y->C[x->mn-im], x->C, im*sizeof(doubleC));
+	      memcpy(y->C, &x->C[im], (x->mn-im)*sizeof(doubleC));
+	    }
+	}
+      else
+	{
+	  im = orient == 's' ? (int) ceil( x->m/2.0 ) : (int) floor( x->m/2.0 );
+	  jm = orient == 's' ? (int) ceil( x->n/2.0 ) : (int) floor( x->n/2.0 );
+	  char *from, *to, *qx1, *qy1, *qx2, *qy2, *qx3, *qy3, *qx4, *qy4;
+	  int elt_size, stride, b12rsize, b34rsize;
+	  if ( y->rc_type == 'r' )
+	    {
+	      elt_size = sizeof(double); qx1 = (char *) x->R; qy1 = (char *) y->R;
+	    }
+	  else
+	    {
+	      elt_size = sizeof(doubleC); qx1 = (char *) x->C; qy1 = (char *) y->C;
+	    }
+	  stride = elt_size*x->m; b12rsize = elt_size*im; b34rsize = elt_size*(x->m-im);
+	  qx4 = qx1 + b12rsize; qx2 = qx1 + jm*stride; qx3 = qx2 + b12rsize;  
+	  qy4 = qy1 + b34rsize; qy2 = qy1 + (x->n-jm)*stride; qy3 = qy2 + b34rsize;  
+	  /* first x quadrant -> third y quadrant */
+	  for ( j = 0, from = qx1, to = qy3 ; j < jm ; j++, from+=stride, to+=stride )
+	    memcpy(to, from, b12rsize);
+	  /* third x quadrant -> first y quadrant */
+	  for ( j = 0, from = qx3, to = qy1 ; j < x->n-jm ; j++, from+=stride, to+=stride )
+	    memcpy(to, from, b34rsize);
+	  /* second x quadrant -> fourth y quadrant */
+	  for ( j = 0, from = qx2, to = qy4 ; j < x->n-jm ; j++, from+=stride, to+=stride )
+	    memcpy(to, from, b12rsize);
+	  /* fourth x quadrant -> second y quadrant */
+	  for ( j = 0, from = qx4, to = qy2 ; j < jm ; j++, from+=stride, to+=stride )
+	    memcpy(to, from, b34rsize);
+	}
+    }
+  else if ( dim_flag == 1 )
+    {
+      im = orient == 's' ? (int) ceil( x->m/2.0 ) : (int) floor( x->m/2.0 );
+      char *from, *to, *qx1, *qy1, *qx2, *qy2;
+      int elt_size, stride, b12rsize, b34rsize;
+      if ( y->rc_type == 'r' )
+	{
+	  elt_size = sizeof(double); qx1 = (char *) x->R; qy1 = (char *) y->R;
+	}
+      else
+	{
+	  elt_size = sizeof(doubleC); qx1 = (char *) x->C; qy1 = (char *) y->C;
+	}
+      stride = elt_size*x->m; b12rsize = elt_size*im; b34rsize = elt_size*(x->m-im);
+      qx2 = qx1 + b12rsize; qy2 = qy1 + b34rsize;
+      /* first im rows of x -> last im rows of y */
+      for ( j = 0, from = qx1, to = qy2 ; j < x->n ; j++, from+=stride, to+=stride )
+	memcpy(to, from, b12rsize);
+      /* last (x->m-im) rows of x -> first (x->m-im) rows of y */
+      for ( j = 0, from = qx2, to = qy1 ; j < x->n ; j++, from+=stride, to+=stride )
+	memcpy(to, from, b34rsize);
+    }
+  else  /* dim_flag == 2 */
+    {
+      jm = orient == 's' ? (int) ceil( x->n/2.0 ) : (int) floor( x->n/2.0 );
+      if ( y->rc_type == 'r' )
+	{
+	  memcpy(&y->R[(x->n-jm)*x->m], x->R, x->m*jm*sizeof(double));
+	  memcpy(y->R, &x->R[jm*x->m], x->m*(x->n-jm)*sizeof(double));
+	}
+      else
+	{
+	  memcpy(&y->C[(x->n-jm)*x->m], x->C, x->m*jm*sizeof(doubleC));
+	  memcpy(y->C, &x->C[jm*x->m], x->m*(x->n-jm)*sizeof(doubleC));
+	}
+     }
+      
+  return y;
+}
+
+
+static int int_nsp_fftshift(Stack stack, int rhs, int opt, int lhs)
+{
+  int dim_flag=0;
+  NspMatrix *x, *y;
+  CheckRhs (1, 2);
+  CheckLhs (1, 1);
+
+  if ((x = GetMat (stack, 1)) == NULLMAT)
+    return RET_BUG;
+
+  if (rhs == 2)
+    if ( GetDimArg(stack, 2, &dim_flag) == FAIL )
+      return RET_BUG;
+
+  if ( dim_flag > 2 )
+    {
+      Scierror("%s: Invalid dim flag '%d' (must be 0, 1 or 2)\n", NspFname(stack), dim_flag);
+      return RET_BUG;
+    }
+  
+  if ( ( y = nsp_mat_shift(x, dim_flag, 's') ) == NULLMAT )
+    return RET_BUG;
+
+  MoveObj (stack, 1, (NspObject *) y);
+  return 1;
+}
+
+static int int_nsp_ifftshift(Stack stack, int rhs, int opt, int lhs)
+{
+  int dim_flag=0;
+  NspMatrix *x, *y;
+  CheckRhs (1, 2);
+  CheckLhs (1, 1);
+
+  if ((x = GetMat (stack, 1)) == NULLMAT)
+    return RET_BUG;
+
+  if (rhs == 2)
+    if ( GetDimArg(stack, 2, &dim_flag) == FAIL )
+      return RET_BUG;
+
+  if ( ( y = nsp_mat_shift(x, dim_flag, 'i') ) == NULLMAT )
+    return RET_BUG;
+
+  if ( dim_flag > 2 )
+    {
+      Scierror("%s: Invalid dim flag '%d' (must be 0, 1 or 2)\n", NspFname(stack), dim_flag);
+      return RET_BUG;
+    }
+
+  MoveObj (stack, 1, (NspObject *) y);
+  return 1;
+}
 
 static OpTab Fft_func[]={
     {"fft", int_nsp_fft},
     {"fft2", int_nsp_fft2},
+    {"fftshift", int_nsp_fftshift},
+    {"ifftshift", int_nsp_ifftshift},
     {(char *) 0, NULL}
 };
 
