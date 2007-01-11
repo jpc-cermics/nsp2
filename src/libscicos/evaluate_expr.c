b3919fa22c11f1f88a2261612572a4a6e5fd4744
Jean-Philippe Chancelier <jpc@cermics.enpc.fr>
2007-01-11 17:23:13 +0000

    updates

--- src/libscicos/evaluate_expr.c
+++ src/libscicos/evaluate_expr.c
@@ -453,119 +453,321 @@ void scicos_evaluate_expr_block_old(scicos_block *block,int flag)
   }
 }
 
-
-#define EVAL(OP) stack[bottom-1]=stack[bottom-1] OP stack[bottom];bottom=bottom-1
-#define UEVAL(OP) stack[bottom]=OP/**/(stack[bottom-1]);
-
-int scicos_evaluate_expr(const int *ipar,int nipar,const double *rpar,double *res)
-{
-  double stack [1000];
-  int count,bottom;
-  bottom=-1; /* position in stack */
-  count=-1;  /* position of arguments in ipar */
-  /* loop on ipar which contains bycode to be executed */
-  while ( count < nipar) 
-    {
-      count=count+1;
-      switch (ipar[count]) 
-	{
-	case 6:
-	  /* [6,xx] : push rpar[ipar[xx]-1] on the stack */
-	  count=count+1;
-	  bottom=bottom+1;
-	  if( bottom > 999 ) {
-	    Scierror("Error: stack overflow in evaluate_expr\n");
-	    return FAIL;
-	  }
-	  stack[bottom]=rpar[ipar[count]-1];
-	  break;
-	case 5:
-	  /* [5,op] : execute op */
-	  count=count+1;
-	  switch (ipar[count]) 
-	    {
-	    case 1: /* + */  EVAL(+);break;
-	    case 2: /* - */  EVAL(-);break;
-	    case 3: /* mult */ EVAL(*);break;
-	    case 7: /* div */ EVAL(/);break;
-	    case 15: /* ^ */ 
-	      stack[bottom-1]=pow(stack[bottom-1],stack[bottom]);
-	      bottom=bottom-1;
-	      break;
-	    case 16: /* == */ EVAL(==);break;
-	    case 17: /* < */  EVAL(<);break;
-	    case 18: /* > */  EVAL(>);break;
-	    case 19: /* <= */ EVAL(<=);break;
-	    case 20: /* >= */ EVAL(>=);break;
-	    case 21: /* != */ EVAL(!=);break;
-	    case 28: /* || */ EVAL(||);break;
-	    case 29: /* && */ EVAL(&&);break;
-	    case 30: /* == 0.0 */
-	      stack[bottom]= (stack[bottom]==0.0) ? 0.0 : 1.0;
-	      break;
-	    case 99: /* -x */
-	      stack[bottom]=-stack[bottom];
-	      break;
-	    case 101: /* sin */ UEVAL(sin);break;
-	    case 102: /* cos */ UEVAL(cos);break;
-	    case 103: /* tan */ UEVAL(tan);break;
-	    case 104: /* exp */ UEVAL(exp);break;
-	    case 105: /* log */ UEVAL(log);break;
-	    case 106: /* sinh */UEVAL(sinh);break;
-	    case 107: /* cosh */UEVAL(cosh);break;
-	    case 108: /* tanh */UEVAL(tanh);break;
-	    case 109: /* round toward 0 */
-	      stack[bottom]=(stack[bottom]>0) ? floor(stack[bottom]) : ceil(stack[bottom]);
-		break;
-	    case 110: /* ? */
-	      stack[bottom]=(stack[bottom]>0) ? floor(stack[bottom]+.5)
-		: ceil(stack[bottom]-.5);
-		break;
-	    case 111: /* ceil */ UEVAL(ceil);break;
-	    case 112: /* floor */ UEVAL(floor);break;
-	    case 113: /* sign */ 
-	      stack[bottom]= (stack[bottom]>0) ? 1.0 : ((stack[bottom]<0) ? -1.0 : 0.0);
-	      break;
-	    case 114:  /* abs */ 
-	      stack[bottom]= Abs(stack[bottom]);
-	      break;
-	    case 115: /* Max */
-	      stack[bottom-1]=Max(stack[bottom-1],stack[bottom]);
-	      break;
-	    case 116: /* Min */
-	      stack[bottom-1]=Min(stack[bottom-1],stack[bottom]);
-	      break;
-	    case 117: /* asin */ UEVAL(asin);break;
-	    case 118: /* acos */ UEVAL(acos);break;
-	    case 119: /* */ UEVAL(atan);break;
-	    case 120: /* */ UEVAL(asinh);break;
-	    case 121: /* */ UEVAL(acosh);break;
-	    case 122: /* */ UEVAL(atanh);break;
-	    case 123: /* */ stack[bottom-1]=atan2(stack[bottom-1],stack[bottom]);
-	      bottom=bottom-1;
-	      break;
-	    case 124: stack[bottom]=log10(stack[bottom]); break;
-	    }
-	}
-    }
-  *res = stack[bottom];
-  return OK;
-}
-
-
+static int nsp_scalarexp_byte_eval_scicos(const int *code,int lcode,const double *constv,const double *vars,
+					  int phase,int flag, int block_ng, double *block_g, int *block_mode,
+					  double *res);
 
 void scicos_evaluate_expr_block(scicos_block *block,int flag)
 {
-  int i;
+  int i,phase;
   double *constv = block->rpar, vars[8],res;
+  phase=get_phase_simulation();  
   if ( block->nin>1 ) 
     for ( i = 0 ; i < block->nin ; i++) vars[i]=block->inptr[i][0];
   else 
     for ( i = 0 ; i < block->insz[0] ; i++) vars[i]=block->inptr[0][i];
-  nsp_scalarexp_byte_eval(block->ipar,block->nipar,constv,vars,&res);
+  nsp_scalarexp_byte_eval_scicos(block->ipar,block->nipar,constv,vars,phase,flag,block->ng,
+				 block->g,block->mode,&res);
   block->outptr[0][0]=res;
 }
 
+
+typedef enum { 
+  f_sin, f_cos, f_tan, f_exp, f_log, f_sinh, f_cosh, f_tanh,
+  f_int, f_round, f_ceil, f_floor, f_sign, f_abs, f_max, f_min,
+  f_asin, f_acos, f_atan, f_asinh, f_acosh, f_atanh,
+  f_atan2, f_log10, f_gamma
+} f_enum;
+
+
+typedef struct _expr_func expr_func;
+
+struct _expr_func {
+  const char *name;
+  f_enum id;
+  double (*f1)(double);
+  double (*f2)(double,double);
+};
+
+
+
+#if WIN32
+double asinh(double x) { return log(x+sqrt(x*x+1));}
+double acosh(double x) { return log(x+sqrt(x*x-1));}
+double atanh(double x) { 
+  return  (x >=0.5) ? 0.5*log((1+x)/(1-x)) : 0.5*log((2*x)+(2*x)*x/(1-x));
+}
+#endif
+
+extern double round(double);
+static double sign(double x) { return (x>0) ? 1: ((x==0) ? 0:-1);}
+static double dmax(double x,double y) { return Max(x,y);}
+static double dmin(double x,double y) { return Min(x,y);}
+static double dabs(double x) { return Abs(x);}
+static double dgamma(double x) {
+#ifdef HAVE_TGAMMA
+  return tgamma(x);
+#else 
+  return cdf_gamma(&x);
+#endif 
+}
+
+
+static expr_func expr_functions[] = 
+  {
+    {"sin",f_sin,sin,NULL},
+    {"cos",f_cos,cos,NULL},
+    {"tan",f_tan,tan,NULL},
+    {"exp",f_exp,exp,NULL},
+    {"log",f_log,log,NULL},
+    {"sinh",f_sinh,sinh,NULL},
+    {"cosh",f_cosh,cosh,NULL},
+    {"tanh",f_tanh,tanh,NULL},
+    {"int",f_int,rint,NULL},
+    {"round",f_round,round,NULL},
+    {"ceil",f_ceil,ceil,NULL},
+    {"floor",f_floor,floor,NULL},
+    {"sign",f_sign,sign,NULL},
+    {"abs",f_abs,dabs,NULL},
+    {"max",f_max,NULL,dmax},
+    {"min",f_min,NULL,dmin},
+    {"asin",f_asin,asin,NULL},
+    {"acos",f_acos,acos,NULL},
+    {"atan",f_atan,atan,NULL},
+    {"asinh",f_asinh,asinh,NULL},
+    {"acosh",f_acosh,acosh,NULL},
+    {"atanh",f_atanh,atanh,NULL},
+    {"atan2",f_atan2,NULL,atan2},
+    {"log10",f_log10,log10,NULL},
+    {"gamma",f_gamma,dgamma,NULL},
+    {NULL,0}
+  };
+
+
+static int nsp_scalarexp_byte_eval_scicos(const int *code,int lcode,const double *constv,const double *vars,
+					  int phase,int flag, int block_ng, double *block_g, int *block_mode,
+					  double *res)
+{
+  unsigned int type;
+  int i,s_pos=0,n, nzcr=-1;
+  double stack[512];
+  for ( i = 0 ; i < lcode ; i++)
+    {
+      unsigned int bcode = *code;
+      code++;
+      type = (bcode & 0xefff0000 ) >> 16;
+      switch (type )
+	{
+	case 1:
+	  n = bcode & 0xffff;
+	  /* Sciprintf("Need  an operator %d\n",n);*/
+	  switch (n) 
+	    {
+	    case TILDE_OP: 
+	      if (flag==9) {
+		block_g[nzcr]=stack[s_pos-1];
+		if(phase==1) {
+		  block_mode[nzcr]=(0.0==stack[s_pos-1]);
+		}
+	      }
+	      if(block_ng>0) nzcr=nzcr+1;
+	      if(phase==1||block_ng==0){
+		i=(stack[s_pos-1]==0.0);
+	      }else{
+		i=block_mode[nzcr];
+	      }
+	      if (i){
+		stack[s_pos-1]=1.0;
+	      }else{
+		stack[s_pos-1]=0.0;   
+	      }
+	      break;
+	    case DOTPRIM :
+	    case QUOTE_OP : break;
+	    case DOTSTARDOT:
+	    case DOTSTAR :
+	    case STAR_OP :  stack[s_pos-2] *= stack[s_pos-1];s_pos--; break;			      
+	    case DOTPLUS: 
+	    case PLUS_OP :  stack[s_pos-2] += stack[s_pos-1];s_pos--; break;			      
+	    case HAT_OP :   stack[s_pos-2]= pow(stack[s_pos-2],stack[s_pos-1]);s_pos--; break;			      
+	    case SEQOR : 	
+	    case OR_OP :    
+	      if(block_ng>0) nzcr=nzcr+1;
+	      if (flag==9) {
+		block_g[nzcr]=stack[s_pos-2]-stack[s_pos-1];
+		if(phase==1) {
+		  block_mode[nzcr]=((int)stack[s_pos-2]||(int)stack[s_pos-1]);
+		}
+	      }
+	      if(phase==1||block_ng==0){
+		i=((int)stack[s_pos-2]||(int)stack[s_pos-1]);
+	      } else{
+		i=block_mode[nzcr];
+	      }
+	      stack[s_pos-2]=(double)i;
+	      s_pos--;
+	      break;
+	    case SEQAND   : 
+	    case AND_OP : 
+	      if(block_ng>0) nzcr=nzcr+1;
+	      if (flag==9) {
+		block_g[nzcr]=stack[s_pos-2]-stack[s_pos-1];
+		if(phase==1) {
+		  block_mode[nzcr]=((int)stack[s_pos-2]&&(int)stack[s_pos-1]);
+		}
+	      }
+	      if(phase==1||block_ng==0){
+		i=((int)stack[s_pos-2]&&(int)stack[s_pos-1]);
+	      } else{
+		i=block_mode[nzcr];
+	      }
+	      stack[s_pos-2]=(double)i;
+	      s_pos--;
+	      break;
+	    case COMMA_OP : break;
+	    case SEMICOLON_OP :break;
+	    case RETURN_OP : break;
+	    case MINUS_OP :  stack[s_pos-2] -= stack[s_pos-1];s_pos--;break; /* binary */
+	    case DOTSLASH:
+	    case DOTSLASHDOT:
+	    case SLASH_OP : stack[s_pos-2] /= stack[s_pos-1];s_pos--;break;
+	    case DOTBSLASH :
+	    case DOTBSLASHDOT: 
+	    case BACKSLASH_OP: stack[s_pos-2] = stack[s_pos-1]/stack[s_pos-2];s_pos--;break;
+	    case DOTHAT : 	stack[s_pos-2]= pow(stack[s_pos-2],stack[s_pos-1]);s_pos--;break;
+	    case DOTEQ :
+	    case EQ     : 
+	      if(block_ng>0) nzcr++;
+	      if (flag==9) {
+		block_g[nzcr]=stack[s_pos-2]-stack[s_pos-1];
+		if(phase==1) {
+		  block_mode[nzcr]=(stack[s_pos-2]== stack[s_pos-1]);
+		}
+	      }
+	      if(phase==1||block_ng==0){
+		i=(stack[s_pos-2]== stack[s_pos-1]);
+	      } else{
+		i=block_mode[nzcr];
+	      }
+	      stack[s_pos-2]=(double) i;
+	      s_pos--;
+	      break;
+	    case DOTLEQ:
+	    case LEQ    :
+	      if(block_ng>0) nzcr=nzcr+1;
+	      if (flag==9) {
+		block_g[nzcr]=stack[s_pos-2]-stack[s_pos-1];
+		if(phase==1) {
+		  block_mode[nzcr]=(stack[s_pos-2]<=stack[s_pos-1]);
+		}
+	      }
+	      if(phase==1||block_ng==0){
+		i=(stack[s_pos-2]<=stack[s_pos-1]);
+	      } else{
+		i=block_mode[nzcr];
+	      }
+	      stack[s_pos-2]=(double)i;
+	      s_pos--;
+	      break;
+	    case DOTGEQ :
+	    case GEQ    : 
+	      if(block_ng>0) nzcr=nzcr+1;
+	      if (flag==9) {
+		block_g[nzcr]=stack[s_pos-2]-stack[s_pos-1];
+		if(phase==1) {
+		  block_mode[nzcr]=(stack[s_pos-2]>=stack[s_pos-1]);
+		}
+	      }
+	      if(phase==1||block_ng==0){
+		i=(stack[s_pos-2]>=stack[s_pos-1]);
+	      } else{
+		i=block_mode[nzcr];
+	      }
+	      stack[s_pos-2]=(double)i;
+	      s_pos--;
+	      break;
+	    case DOTNEQ :
+	    case NEQ    :  
+	      if(block_ng>0) nzcr=nzcr+1;
+	      if (flag==9) {
+		block_g[nzcr]=stack[s_pos-2]-stack[s_pos-1];
+		if(phase==1) {
+		  block_mode[nzcr]=(stack[s_pos-2]!=stack[s_pos-1]);
+		}
+	      }
+	      if(phase==1||block_ng==0){
+		i=(stack[s_pos-2]!=stack[s_pos-1]);
+	      } else{
+		i=block_mode[nzcr];
+	      }
+	      stack[s_pos-2]=(double)i;
+	      s_pos--;
+	      break;
+	    case MOINS   : 	stack[s_pos-1] =-stack[s_pos-1] ;break;   /* unary minus */	      
+	    case DOTLT :
+	    case LT_OP:
+	      if(block_ng>0) nzcr=nzcr+1;
+	      if (flag==9) {
+		block_g[nzcr]=stack[s_pos-2]-stack[s_pos-1];
+		if(phase==1) {
+		  block_mode[nzcr]=(stack[s_pos-2]<stack[s_pos-1]);
+		}
+	      }
+	      if(phase==1||block_ng==0){
+		i=(stack[s_pos-2]<stack[s_pos-1]);
+	      } else{
+		i=block_mode[nzcr];
+	      }
+	      stack[s_pos-2]=(double)i;
+	      s_pos--;
+	      break;
+	    case DOTGT:
+	    case GT_OP: 
+	      if(block_ng>0) nzcr=nzcr+1;
+	      if (flag==9) {
+		block_g[nzcr]=stack[s_pos-2]-stack[s_pos-1];
+		if(phase==1) {
+		  block_mode[nzcr]=(stack[s_pos-2]>stack[s_pos-1]);
+		}
+	      }
+	      if(phase==1||block_ng==0){
+		i=(stack[s_pos-2]>stack[s_pos-1]);
+	      } else{
+		i=block_mode[nzcr];
+	      }
+	      stack[s_pos-2]=(double)i;
+	      s_pos--;
+	      break;
+	  }
+	  break;
+	case 2:
+	  n = bcode & 0xffff;
+	  /* Sciprintf("Need  a function %d\n", n); */
+	  if ( expr_functions[n].f1 != NULL) 
+	    {
+	      stack[s_pos-1] = (expr_functions[n].f1)(stack[s_pos-1]);
+	    }
+	  else 
+	    {
+	      stack[s_pos-2] = (expr_functions[n].f2)(stack[s_pos-2],stack[s_pos-1]);
+	      s_pos--;
+	    }
+	  break;
+	case 3:
+	  /* Sciprintf("Need  a name %d\n", bcode & 0xffff); */
+	  stack[s_pos]=vars[  bcode & 0xffff];
+	  s_pos++;
+	  break;
+	case 4:
+	  /* Sciprintf("A number %f\n",constv[ bcode & 0xffff]); */
+	  stack[s_pos]=constv[ bcode & 0xffff]; 
+	  s_pos++;
+	  break;
+	}
+    }
+  *res = stack[0];
+  return OK;
+}
 
 
 
