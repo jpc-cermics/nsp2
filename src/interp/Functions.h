021508fca0f6388762e6bfbc555e44a6a6f552ac
Jean-Philippe Chancelier <jpc@cermics.enpc.fr>
2007-05-06 16:16:38 +0000

    update

--- src/interp/Functions.h
+++ src/interp/Functions.h
@@ -78,7 +78,7 @@ extern void nsp_init_macro_table(void);
 extern int nsp_enter_macros(const char *dirname,int recursive,int compile);
 extern int nsp_delete_macros(const char *Dir);
 extern const char *nsp_get_libdir(int num);
-
+extern void nsp_macro_table_reset_cache(void);
 
 /* Parse.c */
 
