f65a5e881498b11d61ae8cd2171abb62d59ed853
Jean-Philippe Chancelier <jpc@cermics.enpc.fr>
2007-10-29 16:00:16 +0000

    updates

--- src/libcdf/algdiv.c
+++ src/libcdf/algdiv.c
@@ -1,3 +1,26 @@
+/* Nsp
+ * Copyright (C) 2007 Jean-Philippe Chancelier Enpc/Cermics
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ *
+ * ln(gamma(b)/gamma(a+b)) when b >= 8 a+b too. 
+ *
+ */
+
 #include "cdf.h"
 
 /**
@@ -9,68 +32,13 @@
  * in this algorithm, del(x) is the function defined by 
  * ln(gamma(x)) = (x - 0.5)*ln(x) - x + 0.5*ln(2*pi) + del(x). 
  *     
- * Note: this approximation is used for b >= 8 but certainly also 
- * a+b>=8. 
+ * Note: this approximation is used for b >= 8 and a+b>=8. 
  *      
  * Returns: a double 
  **/
 
-double cdf_algdiv (double a, double b)
-{
-  /* test
-     bs=9;a=-(bs-2):0.1:bs;
-     b=bs*ones(a);norm(cdf_algdiv(a,bs*ones(a))  - log(gamma(b)./gamma(a+b)),'inf')
-  */
-  static const double c0 = .0833333333333333;
-  static const double c1 = -.00277777777760991;
-  static const double c2 = 7.9365066682539e-4;
-  static const double c3 = -5.9520293135187e-4;
-  static const double c4 = 8.37308034031215e-4;
-  static const double c5 = -.00165322962780713;
-  double d1, c, d, h, t, u, v, w, x, s3, s5, s7, x2, s9, s11;
 
-  if (a <= b)
-    {
-      h = a / b;
-      c = h / (h + 1.);
-      x = 1. / (h + 1.);
-      d = b + (a - .5);
-    }
-  else 
-    {
-      h = b / a;
-      c = 1. / (h + 1.);
-      x = h / (h + 1.);
-      d = a + (b - .5);
-    }
-  x2 = x * x;
-  s3 = x + x2 + 1.;
-  s5 = x + x2 * s3 + 1.;
-  s7 = x + x2 * s5 + 1.;
-  s9 = x + x2 * s7 + 1.;
-  s11 = x + x2 * s9 + 1.;
-  /* SET W = DEL(B) - DEL(A + B) */
-  /* Computing 2nd power */
-  d1 = 1. / b;
-  t = d1 * d1;
-  w = ((((c5 * s11 * t + c4 * s9) * t + c3 * s7) * t + c2 * s5) * t + c1 * s3) * t + c0;
-  w *= c / b;
-  /* COMBINE THE RESULTS */
-  d1 = a / b;
-  u = d * cdf_dln1px (d1);
-  v = a * (log (b) - 1.);
-  if (u <= v)
-    {
-      return  w - u - v;
-    }
-  return  w - v - u;
-}
-
-/*
- * a revoir il faut augmenter le nbre de termes et Maple marche plus ....
- */
-
-double cdf_algdiv_old (double a, double b)
+double cdf_algdiv(double a, double b)
 {
   double comp=cdf_stirling_series_diff(b,a),u,v;
   /* compute g(b) - g(b+a), where 
@@ -119,7 +87,7 @@ double cdf_stirling_series_diff(double z, double y)
    * Maple code: 
    * Digits:=17;nm:=6;convert([seq(bernoulli(2*n)/(2*n*(2*n-1)),n=1..nm)],float);
    */
-  #define NN 6
+  #define NN 8
   static const double b[]={.083333333333333333, -.0027777777777777778, 
 			   .00079365079365079365, -.00059523809523809524, 
 			   .00084175084175084175, -.0019175269175269175,
@@ -138,41 +106,85 @@ double cdf_stirling_series_diff(double z, double y)
    * s1= 1; s(n+1)= (1+ (z/(y+z))+ (z/(y+z))^2*s(n));
    * 
    */
-  double sn[NN]={1.0,0},x,x2,w,w2,res;
+  double sn[NN]={1.0,0},x,x0,x1,x2,w,w2,res=0.0;
   int i,nn=NN;
   /* x = z/(z+y); */
-  x=( y >= z) ? ((z/y)/(1.0 + (z/y))) : 1.0 /(1.0 + (y/z)); 
+  x0=(z/y); x0= x0/(1.0 + x0);
+  x1=1.0 /(1.0 + (y/z)); 
+  x=( y >= z) ? x0 : x1;
   x2=x*x;
   w=(1/z);
   w2=w*w;
   for ( i= 1 ; i < nn; i++) sn[i]= (1+x+x2*sn[i-1]);
   /* horner evaluation of sum_{n=1}^{6} b2n*(1/z)^{2*n-1}(sn) */
-  res = sn[nn-1]*b[nn-1]*w2;
-  for ( i= nn-2 ; i>=0; i--) res = ( res + sn[i]*b[i])*w2;
-  res *=( y*x);
+  for ( i= nn-1 ; i>=0; i--) 
+    res = ( res + sn[i]*b[i])*w2;
+  res *= (y*x);
   return  res;
 }
 
-/* log(Gamma(x)) : voir plus haut pour la formule a 10^(-16).
+/**
+ * cdf_algdiv:
+ * @a: a double 
+ * @b: a double
  * 
- *
+ * computation of ln(gamma(b)/gamma(a+b)) when b >= 8 
+ * in this algorithm, del(x) is the function defined by 
+ * ln(gamma(x)) = (x - 0.5)*ln(x) - x + 0.5*ln(2*pi) + del(x). 
+ *     
+ * Note: this approximation is used for b >= 8 but certainly also 
+ * a+b>=8. 
+ *      
+ * Returns: a double 
+ **/
+
+/* acm copyrighted code used for testing only 
+ * 
  */
 
-/*
-with(numapprox);
-with(orthopoly);
-f:= proc(x) log(GAMMA(x));end proc;
-Digits:=70;
-gg:=chebpade(f(x),x=7..8,[10,10]);
-Digits:=17;
-gg:= convert(gg,float);
-n:= numer(gg);
-xx:=coeffs(n,x)[1];
-n:=n/xx;
-d:= denom(gg)/xx;
-f_cheb:= unapply(n/d,x);
-infnorm(f(x)-f_cheb(x),x=6..8);
-codegen[C](hornerform(n/d),optimized);
-codegen[C](hornerform(n/d));
-codegen[C](confracform(n/d));
-*/
+double cdf_algdiv_old (double a, double b)
+{
+  static const double c0 = .0833333333333333;
+  static const double c1 = -.00277777777760991;
+  static const double c2 = 7.9365066682539e-4;
+  static const double c3 = -5.9520293135187e-4;
+  static const double c4 = 8.37308034031215e-4;
+  static const double c5 = -.00165322962780713;
+  double d1, c, d, h, t, u, v, w, x, s3, s5, s7, x2, s9, s11;
+
+  if (a <= b)
+    {
+      h = a / b;
+      c = h / (h + 1.);
+      x = 1. / (h + 1.);
+      d = b + (a - .5);
+    }
+  else 
+    {
+      h = b / a;
+      c = 1. / (h + 1.);
+      x = h / (h + 1.);
+      d = a + (b - .5);
+    }
+  x2 = x * x;
+  s3 = x + x2 + 1.;
+  s5 = x + x2 * s3 + 1.;
+  s7 = x + x2 * s5 + 1.;
+  s9 = x + x2 * s7 + 1.;
+  s11 = x + x2 * s9 + 1.;
+  /* SET W = DEL(B) - DEL(A + B) */
+  /* Computing 2nd power */
+  d1 = 1. / b;
+  t = d1 * d1;
+  w = ((((c5 * s11 * t + c4 * s9) * t + c3 * s7) * t + c2 * s5) * t + c1 * s3) * t + c0;
+  w *= c / b;
+  /* COMBINE THE RESULTS */
+  d1 = a / b;
+  u = d * cdf_dln1px (d1);
+  v = a * (log (b) - 1.);
+  if (u <= v)
+    {
+      return  w - u - v;
+    }
+  return  w - v - u;
+}
