e759d55fec92860e6d2a6c200e8f84ef09c53eab
Jean-Philippe Chancelier <jpc@cermics.enpc.fr>
2007-11-01 19:39:52 +0000

    rewrite

--- src/libcdf/Makefile.amk
+++ src/libcdf/Makefile.amk
@@ -7,7 +7,7 @@ SCIDIR1=..\..
 
 LIBRARY = dcd.lib
 
-OBJSC = DcdInterf.obj dlamch.obj algdiv.obj alngam.obj alnrel.obj \
+OBJSC = DcdInterf.obj dlamch.obj algdiv.obj alngam.obj \
 	bcorr.obj betaln.obj bratio.obj brcmp1.obj brcomp.obj \
 	cdfbet.obj cdfbin.obj cdfchi.obj cdfchn.obj cdff.obj cdffnc.obj cdfgam.obj cdfnbn.obj cdfnor.obj \
 	cdfpoi.obj cdft.obj cumbet.obj cumbin.obj cumchi.obj cumchn.obj cumf.obj cumfnc.obj cumgam.obj \
--- src/libcdf/alngam.c
+++ src/libcdf/alngam.c
@@ -1,7 +1,163 @@
+/* Nsp
+ * Copyright (C) 2007 Jean-Philippe Chancelier Enpc/Cermics
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * a version of log(gamma(x)) approximated with Mapple 
+ *
+ */
+
 #include "cdf.h"
 
+
 /**
- * cdf_alngam:
+ * cdf_alngam_old:
+ * @x: pointer to double 
+ * 
+ * computes the natural logarithm of gamma(x). 
+ * or 0 when gamma(x) is negative. pade approximations 
+ * are computed with Maple.
+ * 
+ * Returns: a double 
+ **/
+
+/* approche log(gamma(x))/(x-2) dans [2,3] */
+
+inline double f_approx_2_3(double x)
+{
+    return((-0.2794541810411736E-2+(-0.1750088178423485E-1+(
+-0.6758249491763692E-2+(0.1888720066674089E-1+(0.7889156408246519E-2+(
+0.2888937267292737E-3+(-0.121317216947609E-4+(0.5753967990547954E-6+(
+-0.2115429901368066E-7+0.4123840082903748E-9*x)*x)*x)*x)*x)*x)*x)*x)*x)/(
+0.1389552237743223E-2+(0.202448732795327E-1+(0.4752910037707498E-1+(
+0.2828438434068512E-1+0.4033042504760442E-2*x)*x)*x)*x));
+}
+
+/* approche log(gamma(x))/(x-2) dans [1.5,2] */
+
+inline double f_approx_15_2(double x)
+{
+    return((-0.3131749855875889E-2+(-0.3083317481667811E-1+(
+-0.3246981457626206E-1+(0.3683597860831141E-1+(0.2802282302892632E-1+(
+0.1667696708820676E-2+(-0.9798138151122817E-4+(0.6554108188827196E-5+(
+-0.3412681424406199E-6+0.9446264568714881E-8*x)*x)*x)*x)*x)*x)*x)*x)*x)/(
+0.1431904787560423E-2+(0.298047281489312E-1+(0.1005584471583368+(
+0.844861749564532E-1+0.1694223676885694E-1*x)*x)*x)*x));
+}
+
+inline double f_approx_1_15(double x)
+{
+  return((-0.5652013301775986E-2+(-0.917908961418217E-1+(-0.2354081919178484+
+(-0.5713581783385663E-1+(0.8136406261691052E-1+(0.1046225196882071E-1+(
+-0.9138733441960895E-3+(0.8904336071089001E-4+(-0.6674994095911582E-5+
+0.2640496384435728E-6*x)*x)*x)*x)*x)*x)*x)*x)*x)/(0.1187377777326479E-2+(
+0.3643944985695971E-1+(0.1824310922229121+(0.2292621661592006+
+0.6866974641221182E-1*x)*x)*x)*x));
+}
+
+double cdf_alngam(double x)
+{
+  const double hln2pi=0.91893853320467274178E0;
+  
+  int i, n;
+  double prod, xx, offset;
+
+  if ( x > 12 ) 
+    {
+      return cdf_stirling_series(x) 
+	+  hln2pi  + (x - .5) * log (x) - x;
+    }
+  else 
+    {
+      if ( x>= 0 && x <= 0.5 ) 
+	{
+	  return f_approx_1_15(x+1)*x - log (x);
+	}
+      else if ( x>= 0 && x <=  1.0 ) 
+	{
+	  return f_approx_15_2(x+1)*(x-1)-log(x);
+	}
+      else if ( x >= 0 && x <= 1.5 ) 
+	{
+	  return f_approx_1_15(x)*(x-1);
+	}
+      else if ( x >= 1.5 && x <= 2.0) 
+	{
+	  return f_approx_15_2(x)*(x-2);
+	}
+      prod = 1.;
+      xx = x;
+      /* bring back x <= 3 */
+      while ( xx > 3 ) 
+	{
+	  xx -= 1;
+	  prod *=xx;
+	}
+      /* now bring x >=2 */
+      while ( xx < 2 ) 
+	{
+	  prod /= xx;
+	  xx += 1.;
+	}
+      /* here we have gamma(x)= prod*gamma(xx) 
+       * and xx is in the range [2,3] 
+       */
+      return ( prod < 0 ) ? 0 : log(prod) + f_approx_2_3(xx)*(xx-2);
+    }
+}
+
+
+/*
+ * Using Maple code for approximation of log(GAMMA(x)) in [2,3] 
+ *
+ 
+  with(numapprox);
+  with(orthopoly);
+  f:= proc(x) log(GAMMA(x))/(x-2);end proc;
+
+  Digits:=60;
+  ggp:=chebpade(f(x),x=2..3,[9,4]);
+  Digits:=20;
+  gg:= convert(ggp,float);
+  gg:= convert(gg,horner);
+  f_approx:= unapply(gg,x);
+  codegen[C](f_approx,optimized);
+
+  f:= proc(x) log(GAMMA(x))/(x-2);end proc;
+  Digits:=60;
+  ggp:=chebpade(f(x),x=1.5..2,[9,4]);
+  Digits:=20;
+  gg:= convert(ggp,float);
+  gg:= convert(gg,horner);
+
+  f_approx:= unapply(gg,x);
+  codegen[C](f_approx,optimized);
+  f:= proc(x) log(GAMMA(x))/(x-1);end proc;
+  Digits:=60;
+  ggp:=chebpade(f(x),x=1..1.5,[9,4]);
+  Digits:=20;
+  gg:= convert(ggp,float);
+  gg:= convert(gg,horner);
+  f_approx:= unapply(gg,x);
+  codegen[C](f_approx,optimized);
+
+*/
+
+/**
+ * cdf_alngam_old:
  * @x: pointer to double 
  * 
  * computes the natural logarithm of gamma(x). 
@@ -15,123 +171,6 @@
  **/
 
 double cdf_alngam_old (double x)
-{
-  const int c9 = 9;
-  const int c4 = 4;
-  const int c5 = 5;
-
-  const double hln2pi=0.91893853320467274178E0;
-  const double scoefn[9] =
-    { 62.003838007127258804, 36.036772530024836321, 20.782472531792126786,
-      6.338067999387272343, 2.15994312846059073, .3980671310203570498, 
-      .1093115956710439502, .0092381945590275995, .0029737866448101651 };
-  const double scoefd[4] =
-    { 62.003838007126989331, 9.822521104713994894, -8.906016659497461257,1. };
-  const double coef[5] =
-    { .083333333333333023564, -.0027777777768818808, 7.9365006754279e-4,
-      -5.94997310889e-4, 8.065880899e-4 };
-
-  int i1, i, n;
-  double ret_val, d1, d2, prod, xx, offset;
-
-  if (!(x <= 6.))
-    {
-      goto L70;
-    }
-  prod = 1.;
-  xx = x;
-  if (!(x > 3.))
-    {
-      goto L30;
-    }
- L10:
-  if (!(xx > 3.))
-    {
-      goto L20;
-    }
-  xx += -1.;
-  prod *= xx;
-  goto L10;
- L20:
- L30:
-  if (!(x < 2.))
-    {
-      goto L60;
-    }
- L40:
-  if (!(xx < 2.))
-    {
-      goto L50;
-    }
-  prod /= xx;
-  xx += 1.;
-  goto L40;
- L50:
- L60:
-  d1 = xx - 2.;
-  d2 = xx - 2.;
-  ret_val =  cdf_devlpl (scoefn, c9, d1) / cdf_devlpl (scoefd, c4, d2);
-  /*     COMPUTE RATIONAL APPROXIMATION TO GAMMA(X) */
-  ret_val *= prod;
-  ret_val = log (ret_val);
-  goto L110;
- L70:
-  offset = hln2pi;
-  /*     IF NECESSARY MAKE X AT LEAST 12 AND CARRY CORRECTION IN OFFSET */
-  /*     ADDED TO PREVENT INT OVERFLOW IN int(12.0E0-x) S. STEER */
-  if (x > 12.)
-    {
-      goto L90;
-    }
-  
-  n = (int) (12. - x);
-  if (!(n > 0))
-    {
-      goto L90;
-    }
-  prod = 1.;
-  i1 = n;
-  for (i = 1; i <= i1; ++i)
-    {
-      prod *= x + (double) (i - 1);
-      /* L80: */
-    }
-  offset -= log (prod);
-  xx = x + (double) n;
-  goto L100;
- L90:
-  xx = x;
-  /*     COMPUTE POWER SERIES */
- L100:
-  /* Computing 2nd power */
-  d2 = xx;
-  d1 = 1. / (d2 * d2);
-  ret_val = cdf_devlpl (coef, c5, d1) / xx;
-  ret_val = ret_val + offset + (xx - .5) * log (xx) - xx;
- L110:
-  return ret_val;
-} 
-
-/**
- * cdf_alngam:
- * @x: pointer to double 
- * 
- * computes the natural logarithm of gamma(x). 
- * Returns 0 when gamma(x) <0 instead of Nan.
- * 
- * if x <= 6.0, then use recursion to get x in [2,3]  
- * then apply rational approximation number 5236 of 
- * hart et al, computer approximations, John Wiley and sons, ny, 1968. 
- * if x > 6.0, then use recursion to get x in [6,12] 
- * then use formula 5423 of the same source. 
- * 
- * Returns: a double.
- **/
-
-/* C-code reorganized by jpc.
- */ 
-
-double cdf_alngam (double x)
 {
   const int c9 = 9;
   const int c4 = 4;
@@ -205,67 +244,3 @@ double cdf_alngam (double x)
       return ( prod < 0 ) ? 0 : log(ret_val);
     }
 }
-
-
-/*
- * Using Maple code for approximation of log(GAMMA(x+2)) in [0,1] 
- *
- 
-  with(numapprox);
-  with(orthopoly);
-  f:= proc(x) log(GAMMA(2+x));end proc;
-  g:= proc(x) f(x)/x;end proc;
-  Digits:=50;
-  am:=0;ap:=10;ad:=10;
-  ggp:=chebpade(g(x),x=(am/ad)..(ap/ad),[9,4]);
-  Digits:=17;
-  gg:= convert(ggp,float);
-  gg:= convert(gg,horner);
-  f_approx:= unapply(x*gg,x);
-  infnorm( f(x)- f_approx(x),x=(am/ad)..(ap/ad));
-  codegen[C](f_approx,optimized);
-  codegen[C](g_cheb,optimized);
-  codegen[C](confracform(n/d));
-
-  double f_approx(x)
-  double x;
-{
-  {
-    return(x*(0.221031420017151+(0.4555848067829791+(0.3164552516488399+(
-0.9190165621078898E-1+(0.1018927139501505E-1+(0.1830006377153302E-3+(
--0.6168325943954862E-5+(0.2963113120297188E-6+(-0.1373138686445392E-7+
-0.4123840082903748E-9*x)*x)*x)*x)*x)*x)*x)*x)*x)/(0.5227994551067566+(
-0.6788312470283883+(0.3140284265354363+(0.6054872437876866E-1+
-0.4033042504760442E-2*x)*x)*x)*x));
-  }
-}
-
-  f_err:=proc(x) local u1,u2; u1:=evalf(f(x),70); u2:=evalf(f_approx(x),17);
-    evalf((u1-u2)/u1,70);
-  end proc;
-  
-  m_err:=proc(am,ap,b,nn)
-    mvm:=am*nn;mvp:=ap*nn;mvd:= b*nn;
-    s:=[seq(i/mvd,i=mvm..mvp)];
-    serr:= map(f_err,s);
-    convert(max(op(serr)),float);
-  end proc;
-  
-  m_err(am,ap,ad,1);
-
-  # original code 
-
-  scoefn :=[ 62.003838007127258804, 36.036772530024836321, 20.782472531792126786,
-  6.338067999387272343, 2.15994312846059073, .3980671310203570498, 
-  .1093115956710439502, .0092381945590275995, .0029737866448101651 ];
-  scoefd :=[ 62.003838007126989331, 9.822521104713994894, -8.906016659497461257,1.];
-  d:= (((scoefd[4]*x+ scoefd[3])*x+ scoefd[2])*x+ scoefd[1]);
-  n:= ((((((((scoefn[9]*x+ scoefn[8])*x + scoefn[7])*x+ scoefn[6])*x+ scoefn[5])*x+ scoefn[4])*x+ scoefn[3])*x+ scoefn[2])*x+ scoefn[1]);
-  f1:= unapply(n/d,x);
-  f_approx:= proc(x) f1(x-2); end proc;
-
-  infnorm(f(x)-f_approx(x),x=(am/ad)..(ap/ad));
-  infnorm(log(f(x))-log(f_approx(x)),x=(am/ad)..(ap/ad));
-  
-
-*/
