ae9fe21a8f0de493562acb80e2a594b0ed8d5583
Bruno Pinçon <bruno.pincon@univ-lorraine.fr>
2007-11-13 10:26:15 +0000

    add spmf.h

---
+++ src/include/nsp/spmf.h
@@ -0,0 +1,12 @@
+#ifndef NSP_INC_LIBSPMF
+#define NSP_INC_LIBSPMF
+
+extern double nsp_log1p(double x);
+extern double nsp_sinpi(double x);
+extern double nsp_gamma(double x);
+extern double nsp_lngamma(double x);
+extern int nsp_kcdf(double x, double *res, int n);
+extern double nsp_kcdflim(double x);
+extern double marsaglia_K(double x, int n);
+
+#endif 
