dae6b31ff7e8dd7a17f8e9fa485e3d785cffa008
Jean-Philippe Chancelier <jpc@cermics.enpc.fr>
2008-01-21 16:00:58 +0000

    updates

--- src/graphics-old/Rec.c
+++ src/graphics-old/Rec.c
@@ -42,7 +42,7 @@
 #include "nsp/math.h"
 #include "nsp/graphics/Graphics.h"
 #include "nsp/graphics/Rec_private.h"
-#include "graphic.h" 
+#include "nsp/graphic.h" 
 
 static int MaybeCopyVect3dPLI  (int ,int **,const int *,int l); 
 static int CopyVectLI  (int **,const int *,int ); 
