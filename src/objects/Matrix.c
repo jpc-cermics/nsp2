feceb9dcb6a3be1f3bda619e4b55cfe15c894360
Bruno Pinçon <bruno.pincon@univ-lorraine.fr>
2008-03-11 07:21:25 +0000

    simplifications in int_impl

--- src/objects/Matrix.c
+++ src/objects/Matrix.c
@@ -181,42 +181,31 @@ NspMatrix *nsp_matrix_create_impl(double first, double step, double last)
 NspMatrix *nsp_matrix_create_int_impl(int first, int step, int last)
 {
   NspMatrix *Loc;
-  int vals = first;
   int count=0;
-  if ( (first < last && step < 0 )
-       || (first >  last && step > 0 )
-       || step == 0.00)
+  if ( (first < last  &&  step < 0) || (first > last && step > 0) || step == 0 )
     {
-      Loc = nsp_matrix_create(NVOID,'r',(int) 1,(int) 0);
-      return(Loc);
+      Loc = nsp_matrix_create(NVOID,'r', 1, 0);
+      return Loc;
     }
+
   /* counting **/
-  if ( step > 0 )
-    {
-      /*       while ( vals <= last ) { vals += step ; count++;} */
-      count = 1 + (int) floor((last-first)/step);
-      vals = first + count*step;
-      if ( vals -last <  Max(Abs(first),Abs(last))*DBL_EPSILON*10) count++;
-    }
-  else if ( step < 0)
-    {
-      /*       while ( vals >= last ) { vals += step ; count++;} */
-      count = 1 + (int) floor((last-first)/step);
-      vals = first + count*step;
-      if ( last - vals <  Max(Abs(first),Abs(last))*DBL_EPSILON*10) count++;
-    }
-  else {
-    Scierror("Error:\t step is 0 in an implicit vector specification\n");
+  if ( step == 1 )
+    count = last - first + 1;
+  else if ( step == -1 )
+    count = first - last + 1;
+  else if ( step > 0 )
+    count = 1 + (last-first)/step;
+  else
+    count = 1 + (first-last)/(-step);
+
+  if ( (Loc = nsp_matrix_create(NVOID,'r',0,0)) == NULLMAT )
     return NULLMAT;
-  }
-  Loc = nsp_matrix_create(NVOID,'r',0,0);
-  if ( Loc == NULLMAT) return(NULLMAT);
   Loc->m=1;
   Loc->n=Loc->mn=count;
   Loc->impl[0]=first;
   Loc->impl[1]=step;
   Loc->convert = 'u';
-  return(Loc);
+  return Loc;
 }
 
 
