ecbb291e92eafb7ac165a70ab9fb4a858a779a59
Jean-Philippe Chancelier <jpc@cermics.enpc.fr>
2009-04-17 19:34:50 +0000

    updates

--- src/gtksci/click.c
+++ src/gtksci/click.c
@@ -125,7 +125,7 @@ int nsp_enqueue(nsp_event_queue *q, nsp_gwin_event *ev)
    *      gives a pb in scicos to be solved 
    */
 
-  Sciprintf("Put event in the queue [%d,%d,%d]\n",ev->x,ev->y,ev->ibutton); 
+  /* Sciprintf("Put event in the queue [%d,%d,%d]\n",ev->x,ev->y,ev->ibutton);  */
   
   if ( ev->motion == 1 || ev->release == 1 ) 
     {
