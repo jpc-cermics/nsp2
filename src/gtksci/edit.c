4b8634d9ccb9feac74585bd3d36fba81ff6f84fe
Jean-Philippe Chancelier <jpc@cermics.enpc.fr>
2010-04-11 07:24:11 +0000

    an editor based on gtksourceview

---
+++ src/gtksci/edit.c
@@ -0,0 +1,28 @@
+/* Nsp
+ * Copyright (C) 2010-2010 Jean-Philippe Chancelier Enpc/Cermics
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * jpc@cermics.enpc.fr 
+ *--------------------------------------------------------------------------*/
+
+#include <nsp/config.h>
+
+#ifdef  HAVE_GTKSOURCEVIEW
+#include "gtksourceview.c"
+#else 
+#include "gtktextview.c"
+#endif 
