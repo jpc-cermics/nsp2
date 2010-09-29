2a6d9a6bbf08f6f471cf36495d798c608ecfb291
Bruno Pinçon <bruno.pincon@univ-lorraine.fr>
2010-09-29 09:18:31 +0000

    maj

--- src/libcdf/new/cdfpoi.c
+++ src/libcdf/new/cdfpoi.c
@@ -49,161 +49,85 @@
  * Returns: 0
  **/
 
+/* modified by Bruno Pincon and Jean-Philippe Chancelier (sept 2010) */
+
 int
 cdf_cdfpoi (int *which, double *p, double *q, double *s, double *xlam,
 	    int *status, double *bound, double *boundbis)
 {
-  const int c__1 = 1;
-  static double c_b22 = 0.;
-  static double c_b23 = .5;
-  static double c_b25 = 5.;
-
   const double tol=1.0E-14, atol=1.0E-50, inf=1.0E300;
-  double d__1;
-  double ccum;
-  int qleft;
-  int qporq;
-  double fx, pq;
-  int qhi;
-  double cum;
+  int pq_flag;
+  double fx, cum, ccum;
 
+  /* check parameters */
   CDF_CHECK_ARG(*which < 1 , 1 , -1 );
   CDF_CHECK_ARG(*which > 3 , 3 , -1 );
  
-  if (*which != 1)
+  if (*which != 1)       /* check p and q */
     {
-      /*     P */
-      CDF_CHECK_ARG(*p < 0 , 0 , -2 );
-      CDF_CHECK_ARG(*p > 1 , 1 , -2 );
-      /*     Q */
-      CDF_CHECK_ARG(*q <= 0 , 0 , -3 );
-      CDF_CHECK_ARG(*q > 1 , 1 , -3 );
-    }
-
-  if (*which != 2 && *which != 1 )
-    {
-      /* add *which==1 to compute cdfpoi with *s < 0 (bruno april,12,2010)) */
-      /*     S */
-      CDF_CHECK_ARG(*s < 0 , 0 , -4 );
-    }
-
-  if (*which != 3)
-    {
-      /*     XLAM */
-      CDF_CHECK_ARG(*xlam < 0 , 0 , -5 );
-    }
-
-  if (*which != 1)
-    {
-      /*     P + Q */
-      pq = *p + *q;
-      if (((d__1 = pq - .5 - .5, Abs (d__1)) > cdf_spmpar (c__1) * 3.))
+      if ( *which == 2 ) /* p=0 and q = 0 are possible */
 	{
-	  *bound = (!(pq < 0.)) ?  1.: 0.0;
-	  *status = 3;
-	  return 0;
+	  CDF_CHECK_PQ( 0.0 <= , <= 1.0 );
 	}
-    }
-
-  if (*which != 1)
-    {
-      /*     P + Q */
-      pq = *p + *q;
-      if (((d__1 = pq - .5 - .5, Abs (d__1)) > cdf_spmpar (c__1) * 3.))
+      else
 	{
-	  * bound = (pq < 0.) ? 0: 1;
-	  *status = 3;
-	  return 0;
+	  CDF_CHECK_PQ( 0.0 < , <= 1.0 );
 	}
+      pq_flag = *p <= *q;
     }
 
-  if ( *which != 1)
+  if (*which != 2 && *which != 1 ) /* add *which==1 to compute cdfpoi with *s < 0 (bruno april,12,2010)) */
     {
-      qporq = *p <= *q;
+      CDF_CHECK_ARG( !(*s >= 0) , 0 , -4 );
     }
 
-  /*     Select the minimum of P or Q */
-  /*     Calculate ANSWERS */
-  if (1 == *which)
+  if (*which != 3)       /* check xlam */
     {
-      /*     Calculating P */
+      CDF_CHECK_ARG(!(*xlam > 0) , 0 , -5 );  /* note: the very special case xlam=0 was accepted before  */
+    }
+
+
+
+  /*  Calculate ANSWERS */
+
+  if (1 == *which)       /* compute (p,q) */
+    {
       double sf = floor(*s);  /* add floor to compute the real cdfpoi (bruno april 2010)) */
       cdf_cumpoi (&sf, xlam, p, q);
       *status = 0;
     }
-  else if (2 == *which)       /*     Calculating S */
-    {
-      /*       if ( *p <= exp(-*xlam) ) bruno april 2010 (using the jpc 's workaround of cdfbin) */
-      /* 	{ */
-      /* 	  *status = 0; */
-      /* 	  *s =0.0; */
-      /* 	  return 0; */
-      /* 	} */
-      /*       *s = *xlam;  start from the mean instead of 5, bruno april 2010 */
-      /*       cdf_dstinv (&c_b22, &inf, &c_b23, &c_b23, &c_b25, &atol, &tol); */
-      /*       *status = 0; */
-      /*       cdf_dinvr (status, s, &fx, &qleft, &qhi); */
-      /*     L200: */
-      /*       if (!(*status == 1)) */
-      /* 	{ */
-      /* 	  goto L230; */
-      /* 	} */
-      /*       cdf_cumpoi (s, xlam, &cum, &ccum); */
-      /*       if (!qporq) */
-      /* 	{ */
-      /* 	  goto L210; */
-      /* 	} */
-      /*       fx = cum - *p; */
-      /*       goto L220; */
-      /*     L210: */
-      /*       fx = ccum - *q; */
-      /*     L220: */
-      /*       cdf_dinvr (status, s, &fx, &qleft, &qhi); */
-      /*       goto L200; */
-      /*     L230: */
-      /*       if (!(*status == -1)) */
-      /* 	{ */
-      /* 	  goto L260; */
-      /* 	} */
-      /*       if (!qleft) */
-      /* 	{ */
-      /* 	  goto L240; */
-      /* 	} */
-      /*       *status = 1; */
-      /*       *bound = 0.; */
-      /*       goto L250; */
-      /*     L240: */
-      /*       *status = 2; */
-      /*       *bound = inf; */
-      /*     L250: */
-      /*     L260: */
-      /*       ; */
 
-      if ( *p <= exp(-*xlam) ) /* bruno april 2010 (using the jpc 's workaround of cdfbin) */
+  else if (2 == *which)  /* compute s */
+    {
+      if ( *q == 1.0 )
 	{
-	  *status = 0;
-	  *s =0.0;
-	  return 0;
+	  *status = 0; *s = 2.0*DBL_MAX; /* Inf but this is valid only if xlam > 0 */
+	}
+      else if ( *p <= exp(-*xlam) ) /* bruno april 2010 (using the jpc 's workaround of cdfbin) */
+	{
+	  *status = 0; *s =0.0;
 	}
       else
 	{
 	  ZsearchStruct S;
 	  zsearch_ret ret_val;
-	  zsearch_monotonicity monotonicity = qporq ? INCREASING : DECREASING;
 	  double step = sqrt(*xlam); /* initial step = std */
 	  *s = *xlam + 0.333;  /* start near the median instead of 5, bruno september 2010 */
-	  nsp_zsearch_init(*s, 0.0, inf, step, 0.0, 2.0, atol, tol, monotonicity, &S);
+	  nsp_zsearch_init(*s, 0.0, inf, step, 0.0, 2.0, atol, tol, pq_flag ?  INCREASING : DECREASING, &S);
 
 	  do
 	    {
 	      cdf_cumpoi (s, xlam, &cum, &ccum);
-	      if ( qporq )
+	      if ( ccum > 1.5 ) /* this comes from gratio (when it is unable to compute the result the result is 2) */
+		{
+		  *status = 10; return 0;
+		}
+	      if ( pq_flag )
 		fx = cum - *p;
 	      else
 		fx = ccum - *q;
-	      ret_val = nsp_zsearch(s, fx, &S);
 	    }
-	  while ( ret_val == EVAL_FX );
+	  while ( (ret_val = nsp_zsearch(s, fx, &S)) == EVAL_FX );
 
 	  switch ( ret_val )
 	    {
@@ -217,53 +141,41 @@ cdf_cdfpoi (int *which, double *p, double *q, double *s, double *xlam,
 	    case RIGHT_BOUND_EXCEEDED:
 	      *status = 2; *bound = inf; break;
 	    default:
-	      *status = 4;
+	      *status = 5;
 	    }
 	}
-
     }
-  else if (3 == *which)
+
+  else if (3 == *which)   /* compute xlam */
     {
-      /*     Calculating XLAM */
+      ZsearchStruct S;
+      zsearch_ret ret_val;
       *xlam = 5.;
-      cdf_dstinv (&c_b22, &inf, &c_b23, &c_b23, &c_b25, &atol, &tol);
-      *status = 0;
-      cdf_dinvr (status, xlam, &fx, &qleft, &qhi);
-    L270:
-      if (!(*status == 1))
+      nsp_zsearch_init(*xlam, 0.0, inf, 2.0, 0.0, 2.0, atol, tol, UNKNOWN, &S);
+	
+      do
 	{
-	  goto L300;
+	  cdf_cumpoi (s, xlam, &cum, &ccum);
+	  if ( ccum > 1.5 ) /* this comes from gratio (when it is unable to compute the result it output 2) */
+	    {
+	      *status = 10; return 0;
+	    }
+	  if ( pq_flag )
+	    fx = cum - *p;
+	  else
+	    fx = ccum - *q;
 	}
-      cdf_cumpoi (s, xlam, &cum, &ccum);
-      if (!qporq)
+      while ( (ret_val = nsp_zsearch(s, fx, &S)) == EVAL_FX );
+
+      switch ( ret_val )
 	{
-	  goto L280;
+	case SUCCESS:
+	  *status = 0; break;
+	case BOTH_BOUND_EXCEEDED:
+	  *status = 4; *bound = 0.0; *boundbis = inf; break;
+	default:
+	  *status = 5;
 	}
-      fx = cum - *p;
-      goto L290;
-    L280:
-      fx = ccum - *q;
-    L290:
-      cdf_dinvr (status, xlam, &fx, &qleft, &qhi);
-      goto L270;
-    L300:
-      if (!(*status == -1))
-	{
-	  goto L330;
-	}
-      if (!qleft)
-	{
-	  goto L310;
-	}
-      *status = 1;
-      *bound = 0.;
-      goto L320;
-    L310:
-      *status = 2;
-      *bound = inf;
-    L320:
-    L330:
-      ;
     }
   return 0;
 }		
