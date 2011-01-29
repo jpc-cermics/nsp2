4c5334b0e4fb14897c9cb6a3f91b643fb5d7f6b7
Jean-Philippe Chancelier <jpc@cermics.enpc.fr>
2011-01-29 08:36:26 +0000

    updates with gtk_about_dialog

--- src/gtksci/about.c
+++ src/gtksci/about.c
@@ -1,3 +1,4 @@
+/* -*-   Encoding: utf-8  -*-  */
 /* Nsp
  * Copyright (C) 1998-2010 Jean-Philippe Chancelier Enpc/Cermics
  *
@@ -24,11 +25,13 @@
 #include <stdlib.h>
 #include <string.h>
 #include <gtk/gtk.h>
-
+#define KEEP_PACKAGE
+#include <nsp/config.h>
 #include "nsp-logo.xpm" 
 #include "nsp-gw.xpm" 
 
-void create_nsp_about(void)
+
+void create_nsp_about_old(void)
 {
   GtkWidget *window = NULL;
   GtkWidget *box1;
@@ -63,4 +66,60 @@ void create_nsp_about(void)
   gtk_widget_destroy(window);
 }
 
+void create_nsp_about(void)
+{
+  GdkPixbuf *pixbuf, *transparent;
+  
+  const gchar *authors[] = {
+    "J.Ph Chancelier",
+    "Bruno Pinçon",
+    "and many more for",
+    "all library and interfaced tools",
+    NULL
+  };
 
+  const gchar *documentors[] = {
+    "J.Ph Chancelier",
+    "F. Delebecque",
+    "Bruno Pinçon",
+    "and many more...",
+    NULL
+  };
+
+  const gchar *license =
+    "This library is free software; you can redistribute it and/or\n"
+    "modify it under the terms of the GNU Library General Public License as\n"
+    "published by the Free Software Foundation; either version 2 of the\n"
+    "License, or (at your option) any later version.\n" 
+    "\n"
+    "This library is distributed in the hope that it will be useful,\n"
+    "but WITHOUT ANY WARRANTY; without even the implied warranty of\n"
+    "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU\n"
+    "Library General Public License for more details.\n"
+    "\n"
+    "You should have received a copy of the GNU Library General Public\n"
+    "License along with the Gnome Library; see the file COPYING.LIB.  If not,\n"
+    "write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,\n"
+    "Boston, MA 02111-1307, USA.\n";
+  
+  pixbuf = NULL;
+  transparent = NULL;
+  pixbuf =gdk_pixbuf_new_from_xpm_data (nsp_logo_xpm);
+  transparent = gdk_pixbuf_add_alpha (pixbuf, TRUE, 0xff, 0xff, 0xff);
+  g_object_unref (pixbuf);
+  /* gtk_about_dialog_set_email_hook (activate_email, NULL, NULL); */
+  /* gtk_about_dialog_set_url_hook (activate_url, NULL, NULL);*/
+  gtk_show_about_dialog (NULL,/*GTK_WINDOW (window),*/
+			 "program-name", "Nsp",
+			 "version", PACKAGE_VERSION,
+			 "copyright", "(C) 2004-2011 The Nsp Team",
+			 "license", license,
+			 "website", "http://cermics.enpc.fr/~jpc/nsp-tiddly/",
+			 "authors", authors,
+			 "documenters", documentors,
+			 "logo", transparent,
+                         "title", "About Nsp",
+			 NULL);
+
+  g_object_unref (transparent);
+}
