a7d02c7d8997d391bf9a140ab5a7cd252f9802c4
Jean-Philippe Chancelier <jpc@cermics.enpc.fr>
2012-11-14 12:05:29 +0000

    updates

--- src/gtksci/about.c
+++ src/gtksci/about.c
@@ -28,49 +28,7 @@
 #define KEEP_PACKAGE
 #include <nsp/config.h>
 #include "nsp-logo.xpm" 
-#include "nsp-gw.xpm" 
-
-
-void create_nsp_about_old(void)
-{
-  GtkWidget *window = NULL;
-  GtkWidget *box1;
-  GtkWidget *label;
-  GtkWidget *pixmapwid;
-  GdkPixmap *pixmap;
-  GdkBitmap *mask;
-
-  window = gtk_dialog_new_with_buttons ("Nsp about",NULL, 0,
-					GTK_STOCK_CLOSE, GTK_RESPONSE_ACCEPT,
-					NULL);
-  gtk_widget_realize(window);
-#ifdef GSEAL_ENABLE
-  box1 = gtk_dialog_get_content_area(GTK_DIALOG(window));
-  /* w = gtk_dialog_get_window(window); */
-#else 
-  box1 = GTK_DIALOG(window)->vbox;
-#endif 
-  
-  pixmap = gdk_pixmap_create_from_xpm_d (window->window, &mask, NULL,
-					 (gchar **) nsp_logo_xpm);
-  pixmapwid =gtk_image_new_from_pixmap(pixmap, mask);
-  g_object_unref(G_OBJECT(pixmap));
-  g_object_unref(G_OBJECT(mask));
-  /* XXXX : A garder ds un coin */ 
-
-  pixmap = gdk_pixmap_create_from_xpm_d (window->window, &mask, NULL,
-					 nsp_gw_xpm);
-  gdk_window_set_icon (window->window, NULL,pixmap,pixmap);
-
-  gtk_container_add (GTK_CONTAINER (box1), pixmapwid);
-  
-  label = gtk_label_new ("Nsp \n  GPL Copyright 2004/2011");
-  gtk_box_pack_start (GTK_BOX (box1), label , FALSE, TRUE, 5);
-
-  gtk_widget_show_all (window);
-  gtk_dialog_run(GTK_DIALOG(window));
-  gtk_widget_destroy(window);
-}
+/* #include "nsp-gw.xpm"  */
 
 void create_nsp_about(void)
 {
