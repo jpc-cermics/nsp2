40d13ac914419829629e889adcd8cd3d7927fcf2
Jean-Philippe Chancelier <jpc@cermics.enpc.fr>
2012-11-14 12:09:08 +0000

    added for the case no help is detected at configure

---
+++ src/gtksci/helpvoid.c
@@ -0,0 +1,30 @@
+/* Nsp
+ * Copyright (C) 1998-2012 Jean-Philippe Chancelier Enpc/Cermics
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * jpc@cermics.enpc.fr 
+ *--------------------------------------------------------------------------*/
+
+#include <nsp/nsp.h> 
+#include <nsp/sciio.h>
+#include <nsp/gtksci.h>
+
+int nsp_help_browser(char *mandir,char *locale,char *help_file) 
+{
+  Sciprintf("Warning: no help browser available for this version of nsp\n");
+  return 0;
+}
