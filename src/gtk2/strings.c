fa1752df5a1197f774bb37ca7d7048e5c25a2535
Jean-Philippe Chancelier <jpc@cermics.enpc.fr>
2013-12-06 09:02:29 +0000

    includes revisited

--- src/gtk2/strings.c
+++ src/gtk2/strings.c
@@ -29,19 +29,18 @@
 #include <gtk/gtk.h> 
 #include <gdk/gdk.h> 
 
-#include <nsp/object.h>
+#include <nsp/nsp.h>
+#include <nsp/matrix.h>
 #include <nsp/smatrix.h>
 #include <nsp/bmatrix.h>
+#include <nsp/mpmatrix.h>
 #include <nsp/hash.h>
 #include <nsp/list.h>
 #include <nsp/cells.h>
 #include <nsp/none.h>
-#include <nsp/mpmatrix.h>
-#include <nsp/matrix.h>
 #include <nsp/file.h>
 #include <nsp/type.h>
 #include <nsp/hobj.h>
-
 
 static void nsp_swap_double_array(double *R,int n);
 /*
