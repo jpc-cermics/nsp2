ea55c7e2e6c89cf18f1397cfdee2ef4e40d2804c
Jean-Philippe Chancelier <jpc@cermics.enpc.fr>
2015-02-11 17:32:19 +0000

    updates

--- src/nspbind/src/printing/genheaders.mli
+++ src/nspbind/src/printing/genheaders.mli
@@ -1,4 +1,27 @@
-val count_newlines : string -> int
-val get_include_code : string -> string -> string -> string
+(***********************************************************************)
+(*                                                                     *)
+(*                           Interface generator                       *)
+(*                                                                     *)
+(*          J.Ph Chancelier, Enpc/Cermics                              *)
+(*                                                                     *)
+(*  Copyright 2012-2014,                                               *)
+(*  Ecole Nationale des ponts et chaussees                             *)
+(*  All rights reserved.                                               *)
+(*                                                                     *)
+(*  This file is distributed under the terms of the BSD License.       *)
+(*                                                                     *)
+(***********************************************************************)
+
+(* $Id$ *)
+
+(* val get_include_code : string -> string -> string -> string *)
+
 val write_header_file :
   Stringarg.object_rec -> bool -> string -> (string, string) Hashtbl.t -> unit
+
+(*
+ Local Variables:
+  compile-command: "cd ../..; make"
+  End:
+*)
+
--- src/nspbind/src/printing/genmethods.mli
+++ src/nspbind/src/printing/genmethods.mli
@@ -14,23 +14,6 @@
 
 (* $Id$ *)
 
-(* 
-val method_tmpl_std : string
-val method_tmpl_boxed : string
-val method_tmpl : Stringarg.object_rec -> string
-val constructor_tmpl_std : string
-val constructor_tmpl_boxed : string
-val constructor_tmpl : Stringarg.object_rec -> string
-val written_overrides_table : (string, string) Hashtbl.t
-val write_method :
-  Stringarg.object_rec ->
-  bool -> Stringarg.function_obj -> string -> bool -> bool -> string * bool
-val find_methods : Stringarg.object_rec -> Stringarg.function_obj list
-val write_methods : Stringarg.object_rec -> bool -> unit
-val find_constructors : Stringarg.object_rec -> Stringarg.function_obj list
-
-*)
-
 val write_methods: Stringarg.object_rec -> bool -> unit;;
 val write_constructors:  Stringarg.object_rec -> bool -> 
   (string, string) Hashtbl.t -> Stringarg.function_obj list;;
