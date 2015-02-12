d782806268208421aec8a83bbdd3779eec47e1de
Jean-Philippe Chancelier <jpc@cermics.enpc.fr>
2015-02-12 20:19:31 +0000

    remove some exported functions

--- src/nspbind/src/printing/genfunctions.mli
+++ src/nspbind/src/printing/genfunctions.mli
@@ -14,9 +14,11 @@
 
 (* $Id$ *)
 
-val check_matchers:  Stringarg.function_params list -> string -> bool * string
+val check_matchers:  Stringarg.function_params list -> 
+  string -> bool * string
 val write_functions: (string, string) Hashtbl.t -> unit;;
-val write_function_table:  Stringarg.function_obj list -> bool -> (string, 'a) Hashtbl.t -> string;;
+val write_function_table:  Stringarg.function_obj list -> 
+  bool -> (string, 'a) Hashtbl.t -> string;;
 val write_function_wrapper:  string ->  bool ->  Stringarg.function_obj ->
   string -> bool -> bool -> (string, string) Hashtbl.t -> string * string
 ;;
