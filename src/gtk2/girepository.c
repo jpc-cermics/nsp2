0e36d5677494d79907f023357d1648df35ea5f6c
Jean-Philippe Chancelier <jpc@cermics.enpc.fr>
2015-02-16 08:52:17 +0000

    wip: support for gobject introspection

--- src/gtk2/girepository.c
+++ src/gtk2/girepository.c
@@ -1,15 +1,28 @@
+/* Nsp
+ * Copyright (C) 2015-2015 Jean-Philippe Chancelier Enpc/Cermics
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
 
-#include <nsp/object.h>
-#include <nsp/smatrix.h>
-#include <nsp/bmatrix.h>
-#include <nsp/hash.h>
-#include <nsp/list.h>
-#include <nsp/cells.h>
-#include <nsp/none.h>
-#include <nsp/mpmatrix.h>
-#include <nsp/matrix.h>
-#include <nsp/file.h>
-#include <nsp/type.h>
-#include <nsp/hobj.h>
+#include <nsp/nsp.h> 
 
+#ifdef WITH_GI 
 #include "codegen/girepository.c"
+
+int nsp_gi_def(void) { return TRUE;}
+#else 
+int nsp_gi_def(void) { return FALSE;}
+#endif 
