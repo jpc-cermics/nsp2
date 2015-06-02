f0f5f1f54375531b1f5619a78235a8c0efbf1b06
Jean-Philippe Chancelier <jpc@cermics.enpc.fr>
2015-06-02 04:40:16 +0000

    nsp name and gtk log handler changed

--- src/gtksci/x_main.c
+++ src/gtksci/x_main.c
@@ -1,5 +1,5 @@
 /* Nsp
- * 
+ *
  * Copyright (C) 2005-2015 Jean-Philippe Chancelier Enpc/Cermics
  *
  * This library is free software; you can redistribute it and/or
@@ -17,13 +17,13 @@
  * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
  * Boston, MA 02111-1307, USA.
  *
- * main 
+ * main
  *--------------------------------------------------------------------------*/
 
 #include <ctype.h>
 #if !defined(__MSC__) && ! defined(__MINGW32__)
 #include <sys/ioctl.h>
-#endif 
+#endif
 #include <sys/stat.h>
 #include <string.h>
 #include <stdio.h>
@@ -47,9 +47,9 @@ extern void nsp_create_main_text_view(void);
 
 /* #define STATUS_BAR 1  */
 
-#ifdef STATUS_BAR 
-static void create_scilab_status(void);
-#endif 
+#ifdef STATUS_BAR
+static void create_nsp_status(void);
+#endif
 
 static void nsp_gtk_gl_init (int *argc,char ***argv);
 static void nsp_create_gtk_toplevel(gint argc, gchar *argv[]);
@@ -58,31 +58,36 @@ static void nsp_set_emacs_key_theme(void) ;
 
 /**
  * nsp_gtk_init:
- * @argc: 
- * @argv: 
- * @no_window: 
- * 
- * Initialize gtk 
+ * @argc:
+ * @argv:
+ * @no_window:
+ *
+ * Initialize gtk
  **/
+
+static void my_log_handler(const gchar *log_domain,
+			   GLogLevelFlags log_level,
+			   const gchar *message,
+			   gpointer user_data);
 
 void nsp_gtk_init(int argc, char **argv,int no_window,int use_textview)
 {
-  if ( no_window == FALSE ) 
+  if ( no_window == FALSE )
     {
       const char *shmid= nsp_getenv("SHMID");
       /* we are using a gtk widget app */
       nsp_in_gtk_window();
       /* initialise gtk */
-#ifdef __APPLE__ 
+#ifdef __APPLE__
       /* avoid a gtk warning about locale */
       gtk_disable_setlocale();
       setlocale(LC_ALL,"");
-#else 
+#else
       gtk_set_locale();
-#endif 
+#endif
       gtk_init(&argc,&argv);
-      /* 
-      g_object_set (gtk_settings_get_default (), 
+      /*
+      g_object_set (gtk_settings_get_default (),
 		    "gtk-enable-mnemonics", TRUE,
 		    "gtk-auto-mnemonics", FALSE,
 		    "gtk-enable-accels", TRUE, NULL);
@@ -92,78 +97,92 @@ void nsp_gtk_init(int argc, char **argv,int no_window,int use_textview)
       /* we are in window mode */
       if ( shmid != NULL )
 	{
-	  /* we build a toplevel widget and 
-	   * zterm will be inserted through socket/plug mechanism 
+	  /* we build a toplevel widget and
+	   * zterm will be inserted through socket/plug mechanism
 	   */
 	  nsp_create_gtk_toplevel(argc,argv);
 	}
-      else 
+      else
 	{
-	  /* we just create a menu which will be inserted 
-	   * in the calling zterm through socket/plug mechanism 
+	  /* we just create a menu which will be inserted
+	   * in the calling zterm through socket/plug mechanism
 	   */
 	  create_plugged_main_menu() ;
 	}
-      /* interaction with a textview 
+      /* interaction with a textview
        *
        */
-      if ( use_textview == TRUE) 
+      if ( use_textview == TRUE)
 	nsp_create_main_text_view();
 
-      /* create a status bar 
-       * FIXME: unsused up to now 
-       * 
+      /* create a status bar
+       * FIXME: unsused up to now
+       *
        */
-#ifdef STATUS_BAR 
-      create_scilab_status();
-#endif 
+#ifdef STATUS_BAR
+      create_nsp_status();
+#endif
       nsp_set_emacs_key_theme() ;
 
     }
   /* signals */
   signal(SIGSEGV,sci_clear_and_exit);
 #if !defined(__MSC__) && ! defined(__MINGW32__)
-  signal(SIGINT,sci_clear_and_exit); 
+  signal(SIGINT,sci_clear_and_exit);
   signal(SIGBUS,sci_clear_and_exit);
   signal(SIGQUIT,sci_clear_and_exit);
   signal(SIGHUP,sci_clear_and_exit);
   signal(SIGUSR1,sci_usr1_signal);
   signal(SIGWINCH, sci_winch_signal);
 #endif
-  /* initialize scilab interp  */
+  /* initialize nsp interp  */
   /* C2F(inisci)(&ini, &memory, &ierr); */
   /* set up terminal size */
   sci_winch_signal(0);
+  /* XXX: change error handler to get rid of critical messages
+   * this should be activated or nor depending on a parameter
+   * i.e activated when compiling in debug mode
+   */
+  g_log_set_default_handler (my_log_handler, NULL);
 }
+
+static void my_log_handler(const gchar *log_domain,
+			   GLogLevelFlags log_level,
+			   const gchar *message,
+			   gpointer user_data)
+{
+
+};
+
 
 /**
  * start_sci_gtk:
- * @void: 
- * 
- * used for starting gtk when 
- * scilab has been called with scilab -nw 
- * and a menu or graphic window is activated 
+ * @void:
+ *
+ * used for starting gtk when
+ * nsp has been called with nsp -nw
+ * and a menu or graphic window is activated
  *
  **/
 
 void start_sci_gtk(void)
 {
   int argc=0;
-  char **argv = NULL; 
+  char **argv = NULL;
   if ( nsp_check_events_activated() == TRUE ) return;
   /* initialise gtk */
-#ifdef __APPLE__ 
+#ifdef __APPLE__
   /* avoid a gtk warning about locale */
   gtk_disable_setlocale();
   setlocale(LC_ALL,"");
-#else 
+#else
   gtk_set_locale();
-#endif 
+#endif
   gtk_init(&argc,&argv);
   nsp_gtk_gl_init (&argc, &argv);
   nsp_activate_gtk_events_check();
   nsp_set_emacs_key_theme();
-  /* locale for LC_NUMERIC must be english type 
+  /* locale for LC_NUMERIC must be english type
    * and starting gtk can change this.
    */
   setlocale(LC_NUMERIC,"C");
@@ -171,13 +190,13 @@ void start_sci_gtk(void)
 
 static void nsp_gtk_gl_init (int *argc,char ***argv)
 {
-#ifdef WITH_GTKGLEXT 
+#ifdef WITH_GTKGLEXT
   gtk_gl_init(argc,argv);
 #endif
 }
 
 /*-------------------------------------------------------
- * color status 
+ * color status
  *-------------------------------------------------------*/
 
 static int screencolor = 1 ; /* default screen color status */
@@ -195,18 +214,18 @@ void setcolordef( int screenc)
 }
 
 /*-------------------------------------------------------
- * status bar 
+ * status bar
  *-------------------------------------------------------*/
 
-#ifdef STATUS_BAR 
+#ifdef STATUS_BAR
 
-static GtkWidget *status = NULL; 
+static GtkWidget *status = NULL;
 
-static void create_scilab_status()
+static void create_nsp_status()
 {
-  GtkWidget *Plug; 
+  GtkWidget *Plug;
   char * plug_info = nsp_getenv("SCIINFO");
-  if ( plug_info == NULL) return ; 
+  if ( plug_info == NULL) return ;
 
   Plug = gtk_plug_new(atoi(nsp_getenv("SCIINFO")));
   status  = gtk_statusbar_new ();
@@ -214,30 +233,30 @@ static void create_scilab_status()
   gtk_widget_show_all(Plug);
 }
 
-void scilab_status_show(char * message)
+void nsp_status_show(char * message)
 {
   gtk_statusbar_pop (GTK_STATUSBAR(status), 1);
   gtk_statusbar_push(GTK_STATUSBAR(status), 1, message);
 }
 
-#endif 
+#endif
 
 /*
- * scilab toplevel widget when zterm widget is plugged 
+ * nsp toplevel widget when zterm widget is plugged
  */
 
 #if !defined(__MSC__) && ! defined(__MINGW32__)
 #include <gdk/gdkx.h>
 #include <gdk/gdkprivate.h>
 #include <gdk/gdkkeysyms.h>
-#endif 
+#endif
 
 static GtkWidget  *window = NULL;
 
 /**
- * we use  shared memory to send back to the calling process 
- * the id of a socket button 
- */ 
+ * we use  shared memory to send back to the calling process
+ * the id of a socket button
+ */
 
 #if !defined(__MSC__) && ! defined(__MINGW32__)
 #include <sys/types.h>
@@ -248,8 +267,8 @@ static char *get_shared(void)
   int shmid;
   char *shm;
   const char *s= nsp_getenv("SHMID");
-  
-  if ( s == NULL) 
+
+  if ( s == NULL)
     {
       perror("cannot getenv SHMID");
       exit(1);
@@ -265,7 +284,7 @@ static char *get_shared(void)
   }
   return shm;
 }
-#endif 
+#endif
 
 
 /*
@@ -276,9 +295,9 @@ static char *get_shared(void)
 static void nsp_create_gtk_toplevel(gint argc, gchar *argv[])
 {
 #if !defined(__MSC__) && ! defined(__MINGW32__)
-  guint32 *xid; 
+  guint32 *xid;
   char * shm = get_shared() ;
-#endif 
+#endif
   GtkWidget *vbox,*menubar, *socket_button;
   gtk_set_locale();
   gtk_init(&argc, &argv);
@@ -300,7 +319,7 @@ static void nsp_create_gtk_toplevel(gint argc, gchar *argv[])
   menubar= create_main_menu( window);
   gtk_box_pack_start(GTK_BOX(vbox),menubar,FALSE,TRUE,0);
 
-  /* a socket in which I will redirect interaction */ 
+  /* a socket in which I will redirect interaction */
   socket_button = gtk_socket_new();
   /*   gtk_widget_set_usize(socket_button,300,100); */
   gtk_box_pack_start(GTK_BOX(vbox), socket_button,TRUE,TRUE,0);
@@ -309,40 +328,38 @@ static void nsp_create_gtk_toplevel(gint argc, gchar *argv[])
   gtk_widget_show_all(window);
   gtk_widget_grab_focus(socket_button);
 #if !defined(__MSC__) && ! defined(__MINGW32__)
-  /* I transmit the socket Id via shared memory  */ 
-  xid = (guint32 *) (shm+1); 
+  /* I transmit the socket Id via shared memory  */
+  xid = (guint32 *) (shm+1);
 #ifdef  GDK_WINDOW_XWINDOW
 #ifdef GSEAL_ENABLE
   *xid = GDK_WINDOW_XWINDOW(gtk_socket_get_plug_window(GTK_SOCKET(socket_button)));
-#else 
-  *xid = GDK_WINDOW_XWINDOW(socket_button->window); 
-#endif 
+#else
+  *xid = GDK_WINDOW_XWINDOW(socket_button->window);
+#endif
 
-#else 
+#else
   *xid = socket_button->window;
 #endif
   *shm = '*' ; /* just to tell that there's something to read */
-#endif 
-} 
+#endif
+}
 
-/* emacs mode for edition 
+/* emacs mode for edition
  *
  */
 
-void nsp_set_emacs_key_theme(void) 
+void nsp_set_emacs_key_theme(void)
 {
   GtkSettings *settings;
   gchar *default_key_theme = NULL;
   /*
-   * check settings for edition 
+   * check settings for edition
    */
   settings = gtk_settings_get_default ();
   if ( settings == NULL) return ;
   g_object_get (settings, "gtk-key-theme-name", &default_key_theme, NULL);
-  if ( default_key_theme == NULL || strcmp(default_key_theme,"Emacs") != 0) 
+  if ( default_key_theme == NULL || strcmp(default_key_theme,"Emacs") != 0)
     gtk_settings_set_string_property (settings,
 				      "gtk-key-theme-name", "Emacs","");
   if ( default_key_theme != NULL)  free(default_key_theme);
 }
-
-
