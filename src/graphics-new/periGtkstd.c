6b718da8838d7024fad94c5020c12276551fb90f
Jean-Philippe Chancelier <jpc@cermics.enpc.fr>
2017-02-03 19:59:41 +0000

    upadtes

--- src/graphics-new/periGtkstd.c
+++ src/graphics-new/periGtkstd.c
@@ -27,9 +27,11 @@
 #include <stdarg.h>
 #include <signal.h>
 #include <gtk/gtk.h>
+#include <epoxy/gl.h>
 #define PERI_PRIVATE 1
 #include "nsp/math.h"
 #include "nsp/sciio.h"
+#include "nsp/opengl.h"
 #include "nsp/graphics-new/periGtk.h"
 /* seams defined on WIN32 with gtkglext */
 #ifdef interface
@@ -57,6 +59,8 @@ static void nsp_configure_wait(BCG *dd);
 static void nsp_drawing_resize(BCG *dd,int width, int height);
 #if GTK_CHECK_VERSION(3,0,0)
 #ifdef PERIGTK3GL
+static void realize_gtk3gl (GtkWidget *widget);
+static void unrealize_gtk3gl (GtkWidget *widget);
 static gint render_callback(GtkGLArea    *area, GdkGLContext *context,  gpointer data);
 #else
 static gint draw_callback(GtkWidget *widget, cairo_t *cr, gpointer data);
@@ -2127,120 +2131,272 @@ static gint draw_callback(GtkWidget *widget, cairo_t *cr, gpointer data)
 
 #if defined(PERIGTK3GL) 
 
-/* coords of a cube */
-static GLfloat cube[8][3] =
-  {{0.0, 0.0, 0.0},
-   {1.0, 0.0, 0.0},
-   { 1.0, 0.0, 1.0},
-   {0.0, 0.0, 1.0},
-   {1.0, 1.0, 0.0},
-   {1.0, 1.0, 1.0},
-   {0.0, 1.0, 1.0},
-   {0.0, 1.0, 0.0}};
+#define TEST_GL
 
-static int faceIndex[6][4] =
-{{0, 1, 2, 3},
-  {1, 4, 5, 2},
-  {4, 7, 6, 5},
-  {7, 0, 3, 6},
-  {3, 2, 5, 6},
-  {7, 4, 1, 0}};
+#if defined(TEST_GL)
 
-void
-drawWireframe(int face)
+enum {
+  X_AXIS,
+  Y_AXIS,
+  Z_AXIS,
+
+  N_AXIS
+};
+
+static float rotation_angles[N_AXIS] = { 0.0 };
+
+static const GLfloat vertex_data[] = {
+ 0.f, 0.5f, 0.f, 1.f,
+ 0.5f, -0.366f, 0.f, 1.f,
+ -0.5f, -0.366f, 0.f, 1.f,
+};
+
+static void
+init_buffers (GLuint *vao_out,
+              GLuint *buffer_out)
 {
-  int i;
-  glBegin(GL_LINE_LOOP);
-  for (i = 0; i < 4; i++)
-    glVertex3fv((GLfloat *) cube[faceIndex[face][i]]);
-  glEnd();
+  GLuint vao, buffer;
+
+  /* we only use one VAO, so we always keep it bound */
+  glGenVertexArrays (1, &vao);
+  glBindVertexArray (vao);
+
+  glGenBuffers (1, &buffer);
+
+  glBindBuffer (GL_ARRAY_BUFFER, buffer);
+  glBufferData (GL_ARRAY_BUFFER, sizeof (vertex_data), vertex_data, GL_STATIC_DRAW);
+  glBindBuffer (GL_ARRAY_BUFFER, 0);
+
+  if (vao_out != NULL)
+    *vao_out = vao;
+
+  if (buffer_out != NULL)
+    *buffer_out = buffer;
 }
 
-void
-drawFilled(int face)
+static GLuint
+create_shader (int type, const char *src)
 {
-  int i;
-  glBegin(GL_POLYGON);
-  for (i = 0; i < 4; i++)
-    glVertex3fv((GLfloat *) cube[faceIndex[face][i]]);
-  glEnd();
-}
+  GLuint shader;
+  int status;
 
+  shader = glCreateShader (type);
+  glShaderSource (shader, 1, &src, NULL);
+  glCompileShader (shader);
 
-void
-setMatrix(void)
-{
-  glMatrixMode(GL_PROJECTION);
-  glLoadIdentity();
-  glOrtho(-2.0, 2.0, -2.0, 2.0, -2.0, 2.0);
-  glMatrixMode(GL_MODELVIEW);
-  glLoadIdentity();
-}
-float ax, ay, az;       /* angles for animation */
-static int stencilOn = 1;
-void
-drawScene(void)
-{
+  glGetShaderiv (shader, GL_COMPILE_STATUS, &status);
+  if (status == GL_FALSE)
+    {
+      int log_len;
+      char *buffer;
 
-  int i;
-  glViewport(0, 0, 100,100);
-  setMatrix();
-  ax = 10.0;
-  ay = -10.0;
-  az = 0.0;
+      glGetShaderiv (shader, GL_INFO_LOG_LENGTH, &log_len);
 
-  glEnable(GL_DEPTH_TEST);
-  glDepthFunc(GL_LEQUAL);
+      buffer = g_malloc (log_len + 1);
+      glGetShaderInfoLog (shader, log_len, NULL, buffer);
 
-  glClearColor(0.0, 0.0, 0.0, 0.0);
-  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+      g_warning ("Compile failure in %s shader:\n%s\n",
+                 type == GL_VERTEX_SHADER ? "vertex" : "fragment",
+                 buffer);
 
-  glPushMatrix();
+      g_free (buffer);
 
-  glRotatef(ax, 1.0, 0.0, 0.0);
-  glRotatef(-ay, 0.0, 1.0, 0.0);
+      glDeleteShader (shader);
 
-  /* all the good stuff follows */
-
-  if (stencilOn) {
-    glEnable(GL_STENCIL_TEST);
-    glClear(GL_STENCIL_BUFFER_BIT);
-    glStencilMask(1);
-    glStencilFunc(GL_ALWAYS, 0, 1);
-    glStencilOp(GL_INVERT, GL_INVERT, GL_INVERT);
-  }
-  glColor3f(1.0, 1.0, 0.0);
-  for (i = 0; i < 6; i++) {
-    drawWireframe(i);
-    if (stencilOn) {
-      glStencilFunc(GL_EQUAL, 0, 1);
-      glStencilOp(GL_KEEP, GL_KEEP, GL_KEEP);
+      return 0;
     }
-    glColor3f(0.0, 0.0, 0.0);
-    drawFilled(i);
 
-    glColor3f(1.0, 1.0, 0.0);
-    if (stencilOn) {
-      glStencilFunc(GL_ALWAYS, 0, 1);
-      glStencilOp(GL_INVERT, GL_INVERT, GL_INVERT);
-    }
-    glColor3f(1.0, 1.0, 0.0);
-    drawWireframe(i);
-  }
-  glPopMatrix();
-
-  if (stencilOn)
-    glDisable(GL_STENCIL_TEST);
-
-  /* end of good stuff */
-
-  /* glutSwapBuffers(); */
+  return shader;
 }
 
+static const char *vertex_shader_code =
+"#version 330\n" \
+"\n" \
+"layout(location = 0) in vec4 position;\n" \
+"uniform mat4 mvp;\n"
+"void main() {\n" \
+"  gl_Position = mvp * position;\n" \
+"}";
+
+static const char *fragment_shader_code =
+"#version 330\n" \
+"\n" \
+"out vec4 outputColor;\n" \
+"void main() {\n" \
+"  float lerpVal = gl_FragCoord.y / 400.0f;\n" \
+"  outputColor = mix(vec4(1.0f, 0.85f, 0.35f, 1.0f), vec4(0.2f, 0.2f, 0.2f, 1.0f), lerpVal);\n" \
+"}";
+
+static void
+init_shaders (GLuint *program_out,
+              GLuint *mvp_out)
+{
+  GLuint vertex, fragment;
+  GLuint program = 0;
+  GLuint mvp = 0;
+  int status;
+
+  vertex = create_shader (GL_VERTEX_SHADER, vertex_shader_code);
+  if (vertex == 0)
+    {
+      *program_out = 0;
+      return;
+    }
+
+  fragment = create_shader (GL_FRAGMENT_SHADER, fragment_shader_code);
+  if (fragment == 0)
+    {
+      glDeleteShader (vertex);
+      *program_out = 0;
+      return;
+    }
+
+  program = glCreateProgram ();
+  glAttachShader (program, vertex);
+  glAttachShader (program, fragment);
+
+  glLinkProgram (program);
+
+  glGetProgramiv (program, GL_LINK_STATUS, &status);
+  if (status == GL_FALSE)
+    {
+      int log_len;
+      char *buffer;
+
+      glGetProgramiv (program, GL_INFO_LOG_LENGTH, &log_len);
+
+      buffer = g_malloc (log_len + 1);
+      glGetProgramInfoLog (program, log_len, NULL, buffer);
+
+      g_warning ("Linking failure:\n%s\n", buffer);
+
+      g_free (buffer);
+
+      glDeleteProgram (program);
+      program = 0;
+
+      goto out;
+    }
+
+  mvp = glGetUniformLocation (program, "mvp");
+
+  glDetachShader (program, vertex);
+  glDetachShader (program, fragment);
+
+out:
+  glDeleteShader (vertex);
+  glDeleteShader (fragment);
+
+  if (program_out != NULL)
+    *program_out = program;
+
+  if (mvp_out != NULL)
+    *mvp_out = mvp;
+}
+
+static void
+compute_mvp (float *res,
+             float  phi,
+             float  theta,
+             float  psi)
+{
+  float x = phi * (G_PI / 180.f);
+  float y = theta * (G_PI / 180.f);
+  float z = psi * (G_PI / 180.f);
+  float c1 = cosf (x), s1 = sinf (x);
+  float c2 = cosf (y), s2 = sinf (y);
+  float c3 = cosf (z), s3 = sinf (z);
+  float c3c2 = c3 * c2;
+  float s3c1 = s3 * c1;
+  float c3s2s1 = c3 * s2 * s1;
+  float s3s1 = s3 * s1;
+  float c3s2c1 = c3 * s2 * c1;
+  float s3c2 = s3 * c2;
+  float c3c1 = c3 * c1;
+  float s3s2s1 = s3 * s2 * s1;
+  float c3s1 = c3 * s1;
+  float s3s2c1 = s3 * s2 * c1;
+  float c2s1 = c2 * s1;
+  float c2c1 = c2 * c1;
+
+  /* initialize to the identity matrix */
+  res[0] = 1.f; res[4] = 0.f;  res[8] = 0.f; res[12] = 0.f;
+  res[1] = 0.f; res[5] = 1.f;  res[9] = 0.f; res[13] = 0.f;
+  res[2] = 0.f; res[6] = 0.f; res[10] = 1.f; res[14] = 0.f;
+  res[3] = 0.f; res[7] = 0.f; res[11] = 0.f; res[15] = 1.f;
+  
+  res[0] = c3c2;  res[4] = s3c1 + c3s2s1;  res[8] = s3s1 - c3s2c1; res[12] = 0.f;
+  res[1] = -s3c2; res[5] = c3c1 - s3s2s1;  res[9] = c3s1 + s3s2c1; res[13] = 0.f;
+  res[2] = s2;    res[6] = -c2s1;         res[10] = c2c1;          res[14] = 0.f;
+  res[3] = 0.f;   res[7] = 0.f;           res[11] = 0.f;           res[15] = 1.f;
+}
+
+static GLuint position_buffer;
+static GLuint program;
+static GLuint mvp_location;
+
+static void
+draw_triangle (void)
+{
+  float mvp[16];
+
+  g_assert (position_buffer != 0);
+  g_assert (program != 0);
+
+  compute_mvp (mvp,
+               rotation_angles[X_AXIS],
+               rotation_angles[Y_AXIS],
+               rotation_angles[Z_AXIS]);
+
+  glUseProgram (program);
+  glUniformMatrix4fv (mvp_location, 1, GL_FALSE, &mvp[0]);
+
+  glBindBuffer (GL_ARRAY_BUFFER, position_buffer);
+  glEnableVertexAttribArray (0);
+  glVertexAttribPointer (0, 4, GL_FLOAT, GL_FALSE, 0, 0);
+
+  glDrawArrays (GL_TRIANGLES, 0, 3);
+
+  glDisableVertexAttribArray (0);
+  glUseProgram (0);
+}
+#endif
+
+static void
+realize_gtk3gl (GtkWidget *widget)
+{
+  gtk_gl_area_make_current (GTK_GL_AREA (widget));
+#if defined(TEST_GL)
+  init_buffers (&position_buffer, NULL);
+  init_shaders (&program, &mvp_location);
+#endif
+}
+
+static void
+unrealize_gtk3gl (GtkWidget *widget)
+{
+#if defined(TEST_GL)
+  gtk_gl_area_make_current (GTK_GL_AREA (widget));
+
+  glDeleteBuffers (1, &position_buffer);
+  glDeleteProgram (program);
+#endif
+}
 
 
 static gint render_callback(GtkGLArea    *area, GdkGLContext *context,  gpointer data)
 {
+#if defined(TEST_GL)
+  glClearColor (0.5, 0.5, 0.5, 1.0);
+  glClear (GL_COLOR_BUFFER_BIT);
+
+  draw_triangle ();
+  
+  glFlush ();
+  
+  return TRUE;
+#else
+  
   GdkRectangle *rect;
   BCG *dd = (BCG *) data;
   g_return_val_if_fail(dd != NULL, FALSE);
@@ -2248,12 +2404,6 @@ static gint render_callback(GtkGLArea    *area, GdkGLContext *context,  gpointer
   
   if (gtk_gl_area_get_error (area) != NULL)
     return FALSE;
-
-  drawScene();
-  glFlush ();
-  
-  return TRUE;
-
   
   /*   rect = ( event != NULL) ? &dd->private->invalidated : NULL; */
   /*
@@ -2261,7 +2411,7 @@ static gint render_callback(GtkGLArea    *area, GdkGLContext *context,  gpointer
    */
   rect = NULL;
   
-  if ( dd->private->resize != 0)
+  if (0 && dd->private->resize != 0)
     {
       /* redraw after resize
        */
@@ -2308,6 +2458,7 @@ static gint render_callback(GtkGLArea    *area, GdkGLContext *context,  gpointer
   glFlush ();
   
   return TRUE;
+#endif
 }
 #endif
 
