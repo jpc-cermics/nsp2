64c1840d9985624c16ba32563f4709aee72ad67f
Jean-Philippe Chancelier <jpc@cermics.enpc.fr>
2019-04-06 09:57:00 +0000

    detect libqhull/io.h qhull/io.h libqhull/qh-io.h libqhull/qh-io.h

--- src/include/nsp/qhull.h
+++ src/include/nsp/qhull.h
@@ -12,14 +12,24 @@
 #include <qhull/qset.h>
 #include <qhull/geom.h>
 #include <qhull/poly.h>
+#ifdef HAVE_QHULL_IO_H
 #include <qhull/io.h>
+#endif
+#ifdef HAVE_QHULL_QH_IO_H
+#include <qhull/qh-io.h>
+#endif
 #else
 #ifdef HAVE_LIBQHULL_LIBQHULL_H
 #include <libqhull/libqhull.h>
 #include <libqhull/qset.h>
 #include <libqhull/geom.h>
 #include <libqhull/poly.h>
+#ifdef HAVE_LIBQHULL_IO_H
 #include <libqhull/io.h>
+#endif
+#ifdef HAVE_LIBQHULL_QH_IO_H
+#include <libqhull/qh-io.h>
+#endif
 #endif
 #endif
 
